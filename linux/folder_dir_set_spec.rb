ab239c8cffb7317428925ca61862989f95a1fd66
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-10-13 00:13:42 -0400

    intermediate revision - added correct patch folder expectation

---
+++ linux/folder_dir_set_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+context 'verify set of patch directories' do
+  # some applications notably the wso2 store patches in their staging directory
+  # https://docs.wso2.com/display/Carbon420/Applying+a+Patch+to+the+Kernel
+  path_to_dir = '/tmp/patches'
+  patches = %w|
+    patch_1001
+    patch_1002
+    patch_1010
+    patch_1011
+  |
+
+  before(:each) do
+    Specinfra::Runner::run_command( <<-EOF
+      path_to_dir='#{path_to_dir}'
+      patches='#{patches.join(' ')}'
+      for folder in ${patches} ; do 
+        echo $folder
+        mkdir -p "${path_to_dir}/${folder}"
+      done
+      mkdir -p "${path_to_dir}/dummy"
+    EOF
+  )
+  end
+
+  describe file(path_to_dir) do
+    it { should be_a_directory }
+    # count the patch difectories in the directory
+    it 'should contain set of patches in staging directory' do
+      patch_glob = 'patch*' # to prevent dummy from being globbed
+      folder_list = Dir.glob("#{path_to_dir}/#{patch_glob}").map {|entry| entry.gsub(Regexp.new('^.*/'),'') }
+      $stderr.puts folder_list 
+      (folder_list - patches).should eq []
+    end
+  end
+end
