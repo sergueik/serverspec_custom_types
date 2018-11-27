9cdf75b9d9e97786748710199c87341af30f0e92
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-11-27 01:15:57 -0500

    intermediate revision - provided mock and both positive and netative outcome scenarios of perm fix

--- linux/file_perms_spec.rb
+++ linux/file_perms_spec.rb
@@ -1,51 +1,95 @@
 require 'spec_helper'
 require 'find'
 
-# List of world-writable or world-executable files in specific application directory
-
+# https://serverfault.com/questions/48109/how-to-find-files-with-incorrect-permissions-on-unix
 context 'App word writable executable files' do
-  describe 'File detection' do
+  context 'Unwanted permissions file detection' do
     $DEBUG = false
     result = true
     total_count = 0
     basedir = '/tmp'
-    if $DEBUG
-      basedir = ENV["HOME"]
-      if basedir.nil?
-        basedir = ENV["USERPROFILE"]
-      end
+    before(:each) do
+      Specinfra::Runner::run_command( <<-EOF
+        pushd #{basedir}
+        touch bad1.txt
+        touch bad2.txt
+        chmod 746 bad1.txt
+        chmod 747 bad2.txt
+        1>&2 ls -l bad1.txt bad2.txt
+        popd
+        sleep 3
+      EOF
+      )
     end
-    $stderr.puts "Scanning #{basedir}"
-    Find.find(basedir) do |filepath|
-      unless FileTest.directory?(filepath)
-        begin	
-          s = File.stat(filepath)
-          if (s.mode) & 03 != 0
-            msg = sprintf('%s %o', filepath, s.mode )
-            $stderr.puts msg
-            result = false
-            total_count = total_count + 1
-          end
-        rescue => e
-          $stderr.puts e.to_s
-        end	
+    # NOTE: fix possible poor synchronization between `before` through sleep - not very reliable
+    context 'test' do
+      $stderr.puts "Scanning #{basedir}"
+      Find.find(basedir) do |filepath|
+        unless FileTest.directory?(filepath)
+          begin	
+            s = File.stat(filepath)
+            $stderr.puts sprintf('%s %o', filepath, s.mode )
+            check = s.mode & 03
+            if check != 0
+              result = false
+              total_count = total_count + 1
+            end
+          rescue => e
+            $stderr.puts e.to_s
+          end	
+        end
       end
+      $stderr.puts "total count: #{total_count}"
+      $stderr.puts result
+      it { result.should be_falsy }
+      it { expect(total_count).to be >= 2  }
     end
-
-    $stderr.puts "total count: #{total_count}"
-    $stderr.puts result
-
-    $stderr.puts result
-    it { result.should be_truthy }
+  end  
+  context 'File permissions file fix validation' do
+    $DEBUG = false
+    result = true
+    total_count = 0
+    basedir = '/tmp'
+    before(:each) do
+      Specinfra::Runner::run_command( <<-EOF
+        pushd #{basedir}
+        find . -type f -and \\( -perm /o=w -or -perm /o=x \\) -exec chmod o-wx -c {} \\;
+        popd
+        sleep 3
+      EOF
+      )
+    end
+    context 'test' do
+      $stderr.puts "Scanning #{basedir}"
+      Find.find(basedir) do |filepath|
+        unless FileTest.directory?(filepath)
+          begin	
+            s = File.stat(filepath)
+            check = s.mode & 03
+            if check != 0
+              msg = sprintf('%s %o %d', filepath, s.mode, check )
+              $stderr.puts msg
+              result = false
+              total_count = total_count + 1
+            end
+          rescue => e
+            $stderr.puts e.to_s
+          end	
+        end
+      end
+      $stderr.puts "total count: #{total_count}"
+      $stderr.puts result
+      it { result.should be_truthy }
+    end
   end
 end
 
-# pushd /tmp; touch test.aug ; touch bad.txt; chmod 746 test.aug ;  chmod 747 bad.txt ;  popd
-# ls -l   bad.txt test.aug
-# -rwxr--rwx. 1 root root   0 Nov 26 22:54 bad.txt
-# -rwxr--rw-. 1 root root 270 Nov 23 01:56 test.aug
-# popd
-# https://serverfault.com/questions/48109/how-to-find-files-with-incorrect-permissions-on-unix
-# pushd /tmp ; find  . -type f -and \( -perm /o=w -or -perm /o=x \)  -exec chmod o-wx -c {} \; ; popd
-# mode of ‘./bad.txt’ changed from 0747 (rwxr--rwx) to 0744 (rwxr--r--)
-# mode of ‘./test.aug’ changed from 0746 (rwxr--rw-) to 0744 (rwxr--r--)
+# Puppet would do it like
+#
+# exec { 'correct permissions':
+#   command   =>  'find . -type f -and \( -perm /o=w -or -perm /o=x \) -exec chmod o-wx -c {} \;'
+#   path      => 'usr/bin:/bin',
+#   cwd       => $applcation_directory,
+#   logoutput => on_failure,
+#   unless    => 'test $(find . -type f -and \( -perm /o=w -or -perm /o=x \)) -eq 0'
+# }
