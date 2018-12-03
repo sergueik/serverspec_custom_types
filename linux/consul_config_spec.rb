64f62233687f8e7b6d610cdf78afb147b6ca112a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-12-03 15:16:25 -0500

    intermediate revision -  fixed the unbalanced, added full working minimal config.json example, reenabled unless conditions

--- linux/consul_config_spec.rb
+++ linux/consul_config_spec.rb
@@ -12,16 +12,18 @@ context 'Consul Response Headers' do
 
   consul_config_dir = '/etc/consul.d'
   consul_config_file = "#{consul_config_dir}/config.json"
-
-  describe 'Service checks' do
-    {
+  custom_headers = {
       'Access-Control-Allow-Origin' => '*',
-      'X-Frame-Options' => 'sameorigin, allow-from https://example.com/'
-    }.each do |name, value|
+      'X-Frame-Options'             => 'sameorigin, allow-from https://example.com/',
+      'X-Xss-Protection'            => '1; mode=block'
+  }
+  
+  describe 'Service checks' do
+    custom_headers.each do |name, value|
       describe command(<<-EOF
         curl -I -X GET http://localhost:8500/v1/health/state/any
       EOF
-      ), Specinfra::Runner::run_command("ps ax | grep consu[l]").exit_status.eql?( 0 ) do
+      ), Specinfra::Runner::run_command("ps ax | grep consu[l]").exit_status.eql?(0) do
         its(:stdout) { should contain "#{name}: #{value}" }
       end
     end
@@ -36,37 +38,47 @@ context 'Consul Response Headers' do
       # mock of the /etc/consul.d/service_web_config.json
       # https://www.consul.io/docs/agent/options.html
       # NOTE: modifiers very fragile - does not work here either
-      # unless Specinfra::Runner::run_command("ps ax | grep consu[l]").exit_status.eql?( 0 ) do
-      $stderr.puts "Creaing mock of the #{consul_config_file}"
-      Specinfra::Runner::run_command(<<-EOF
-        ps ax | grep -q consu[l]
-        if [ $? -ne 0 ] ; then
-          1>2 echo "Creaing mock of the #{consul_config_file}"
-          1 > /dev/null 2> /dev/null mkdir '#{consul_config_dir}'
-          cat <<END>#{consul_config_file}
+      unless Specinfra::Runner::run_command("ps ax | grep consu[l]").exit_status.eql?(0)
+        $stderr.puts "Creaing mock of the #{consul_config_file}"
+        Specinfra::Runner::run_command(<<-EOF
+          ps ax | grep -q consu[l]
+          if [ $? -ne 0 ] ; then
+            1>2 echo "Creaing mock of the #{consul_config_file}"
+            1 > /dev/null 2> /dev/null mkdir '#{consul_config_dir}'
+            # Example full config.json
+            cat <<END>#{consul_config_file}
 {
-
+  "datacenter": "dc",
+  "data_dir": "/opt/consul",
+  "log_level": "INFO",
+  "node_name": "discovery",
+  "server": true,
+  "watches": [
+    {
+      "type": "checks",
+      "handler": "/usr/bin/dummy-health-check-handler.sh"
+    }
+  ],
   "ports": {
-    "https": -1,
-    "http": 8500
+    "http": 8500,
+    "https": -1
   },
   "http_config": {
     "response_headers": {
-      "Access-Control-Allow-Origin": "*"
+      "X-XSS-Protection": "1; mode=block",
+      "X-Frame-Options": "sameorigin, allow-from https://example.com/",
+      "Access-Control-Allow-Origin": "*"            
     }
   }
 }
 END
-  fi
-  # shell HEREDOC delimiter must stay in the first column
-      EOF
-      )
+    fi
+    # shell HEREDOC delimiter must stay in the first column
+        EOF
+        )
       end
     end
-    {
-      'Access-Control-Allow-Origin' => '*',
-      'X-Frame-Options' => 'sameorigin, allow-from https://example.com/'
-    }.each do |name, value|
+    custom_headers.each do |name, value|
       # https://stedolan.github.io/jq/manual/#example27	
       describe command(<<-EOF
         jq -r '.http_config.response_headers| to_entries[] | [.key, .value] | @csv' '#{consul_config_file}'
@@ -78,5 +90,4 @@ END
       end
     end
   end
-# TODO: split: rspec reports extra end block
-# end
+end
