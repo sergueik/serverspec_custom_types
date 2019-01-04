c4b8f6f4cfdfe2454bd6c6c9415748e0a21ad60e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-01-04 18:40:38 -0500

    intermediate revision - added sample fragments of common provisioned jenkins job config changes

--- linux/jenkins_job_inspection_spec.rb
+++ linux/jenkins_job_inspection_spec.rb
@@ -20,6 +20,42 @@ context 'Jenkins Artifactory Job Command' do
        # \&quot;ARTIFACTORY_BUILD_NAME\&quot;
        its(:content) {should match Regexp.new('\\\\&quot;' + artifactory_build_name + '\\\\&quot;')}
      end
+     # commont xpath locators are:
+     # '//builders/hudson.tasks.Shell/command/text()' for the plain shell command step
+     #
+     # '//husdon.model.ChoiceParameterDefinition[description[text()="Critical Parameter"]]/choices'
+     # for the hardcoded domain-specific critical choice options
+     #    <husdon.model.ChoiceParameterDefinition>
+     #      <name>parameter</name>
+     #      <description>Critical Parameter</description>
+     #      <choices class="java.util.Arrays$ArrayList">
+     #        <a class="string-array">
+     #          <string>FOO</string>
+     #          <string>BAR</string>
+     #        </a>
+     #      </choices>
+     #    </husdon.model.ChoiceParameterDefinition>
+     # '//org.biouno.unochoice.ChoiceParameter[description[text()="Description of the Parameter"]]/script[@class="org.biouno.unochoice.model.Groovyscript"]/script/text()'
+     # for some domain-specific critical job uno parameter that looks like
+     #
+     # <org.biouno.unochoice.ChoiceParameter plugin="uno-hoice@0.24">
+     #   <name>ParameterName</name>
+     #   <description>Description of the Parameter</description>
+     #   <script class="org.biouno.unochoice.model.Groovyscript">
+     #     <script>
+     #       def res = 'PARAMETER'
+     #     </script>
+     #   </script>
+     # </org.biouno.unochoice.ChoiceParameter>
+     #
+
+     describe command( <<-EOF
+       xmllint xmllint --xpath '//script/text()' '#{job_dir}/config.xml'
+     EOF
+     ) do
+        # Note: the HTML encoding will disappear after piping through xmllint
+       its(:stdout) {should match Regexp.new('\\\\"' + artifactory_build_name + '\\\\"')}
+     end
      describe file ("#{job_dir}/nextBuildNumber") do
        it { expect(subject).to be_file }
      end
