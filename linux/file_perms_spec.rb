e6f37ccfa967dfc5658a4dc8c558120c6588bd14
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-03-12 17:27:12 -0400

    intermedite revision - added flexible perm bit

--- linux/file_perms_spec.rb
+++ linux/file_perms_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 require 'find'
 
+$DEBUG = (ENV.fetch('DEBUG', false) =~ (/^(true|t|yes|y|1)$/i))
+
 # https://serverfault.com/questions/48109/how-to-find-files-with-incorrect-permissions-on-unix
-context 'App word writable executable files' do
-  context 'Unwanted permissions file detection' do
-    $DEBUG = false
+context 'Permission scan' do
+  context 'world-writable and world-executable file detection' do
     result = true
     total_count = 0
     basedir = '/tmp'
@@ -44,9 +45,8 @@ context 'App word writable executable files' do
       it { result.should be_falsy }
       it { expect(total_count).to be >= 2  }
     end
-  end  
+  end
   context 'File permissions file fix validation' do
-    $DEBUG = false
     result = true
     total_count = 0
     basedir = '/tmp'
@@ -61,12 +61,18 @@ context 'App word writable executable files' do
     end
     context 'test' do
       $stderr.puts "Scanning #{basedir}"
+      # NOTE: this will fail for combination of undesired permission bits detected
+      perms = {1 => 'execute' , 2 => 'write' 4 => 'read' }
       Find.find(basedir) do |filepath|
         unless FileTest.directory?(filepath)
           begin	
             s = File.stat(filepath)
-            check = s.mode & 03
+            # read | write| execute
+            # check = s.mode & 03
+            # sometimes security requests an 750 "or better offer" permission on every file
+            check = s.mode & 07
             if check != 0
+              # msg = sprintf('%s %o %s | %d', filepath, s.mode, perms[check], check )
               msg = sprintf('%s %o %d', filepath, s.mode, check )
               $stderr.puts msg
               result = false
@@ -84,12 +90,13 @@ context 'App word writable executable files' do
   end
 end
 
-# Puppet would do it like
+# In Puppet one would possibly do it like
 #
 # exec { 'correct permissions':
-#   command   =>  'find . -type f -and \( -perm /o=w -or -perm /o=x \) -exec chmod o-wx -c {} \;'
+#   command   => 'find . -type f -and \( -perm /o=w -or -perm /o=x \) -exec chmod o-wx -c {} \;'
 #   path      => 'usr/bin:/bin',
 #   cwd       => $applcation_directory,
 #   logoutput => on_failure,
 #   unless    => 'test $(find . -type f -and \( -perm /o=w -or -perm /o=x \)) -eq 0'
 # }
+# with n obvious modification of the file mode is required to be set to 750 obo
