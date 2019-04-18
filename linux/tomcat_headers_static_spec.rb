d0d05978fe565befb1bcf605f98898935e18aaf3
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-04-18 19:07:36 -0400

    intermediate revision - added the actual impementation prototype

--- linux/tomcat_headers_static_spec.rb
+++ linux/tomcat_headers_static_spec.rb
@@ -9,14 +9,6 @@ require 'spec_helper'
 # https://webmasters.stackexchange.com/questions/37855/tomcat-serving-static-content-with-directory-listings
 # but the enterprise version could be locked
 # 
-# curl -I -k http://localhost:8080/index.jsp
-# HTTP/1.1 200
-# X-Frame-Options: DENY
-# X-Content-Type-Options: nosniff
-# X-XSS-Protection: 1; mode=block
-# Content-Type: text/html;charset=UTF-8
-# Transfer-Encoding: chunked
-# Date: Thu, 18 Apr 2019 19:34:39 GMT
 
 context 'Tomcat static page test' do
 
@@ -37,6 +29,7 @@ EOF
    before(:each) do
     $stderr.puts "Writing #{static_page_datafile}"
     Specinfra::Runner::run_command( <<-EOF
+      mkdir -p $(dirname #{static_page_datafile})
       # no space at beginning of the document is critical for xml
       cat<<DATA1|tee #{static_page_datafile}
 #{static_page_content}
@@ -54,8 +47,32 @@ DATA1
     set /augeas/load/xml/lens "Xml.lns"
     set /augeas/load/xml/incl "#{xml_file}"
     load
-    print /files#{xml_file}/#{aug_path}
+    insert 'Context' after '/files/opt/tomcat/conf/server.xml/Server/Service/Engine/Host[#attribute/name="localhost"]/Valve'
+    save
+    set '/files/opt/tomcat/conf/server.xml/Server/Service/Engine/Host[#attribute/name="localhost"]/Context/#attribute/path' '/static'
+    set '/files/opt/tomcat/conf/server.xml/Server/Service/Engine/Host[#attribute/name="localhost"]/Context/#attribute/docBase' '/var/static'
+    save
   EOF
+    # curl -I -k http://localhost:8080/index.jsp
+    # HTTP/1.1 200
+    # X-Frame-Options: DENY
+    # X-Content-Type-Options: nosniff
+    # X-XSS-Protection: 1; mode=block
+    # Content-Type: text/html;charset=UTF-8
+
+    # HTTP/1.1 404
+    # X-Frame-Options: SAMEORIGIN
+    # X-Content-Type-Options: nosniff
+    # X-XSS-Protection: 1; mode=block
+    # Content-Type: text/html;charset=utf-8
+
+    # HTTP/1.1 200
+    # X-Frame-Options: SAMEORIGIN
+    # X-Content-Type-Options: nosniff
+    # X-XSS-Protection: 1; mode=block
+    # Accept-Ranges: bytes
+    # ETag: W/"62-1555625608000"
+    # 
   describe command(<<-EOF
     echo '#{program}' > #{aug_script}
     augtool -f #{aug_script}
@@ -65,6 +82,21 @@ DATA1
     its(:stdout) { should match '.*#attribute/name = "localhost"' }
     its(:stderr) { should be_empty }
     its(:exit_status) {should eq 0 }
+  end
+  
+  describe command(<<-EOF
+    /opt/tomcat/bin/shutdown.sh
+    /opt/tomcat/bin/shutdown.sh
+    sleep 30
+    curl -k -I http://localhost:8080/static/#{static_page}   
+  EOF
+  ) do
+    its(:stdout) { should contain 'X-Frame-Options: SAMEORIGIN' }
+    its(:stderr) { should be_empty }
+    its(:exit_status) {should eq 0 }
+
+    # curl -I -k http://localhost:8080/index.jsp
+
   end
 end
 
