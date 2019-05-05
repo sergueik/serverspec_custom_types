84c17105492b7f66d34ea6675ba61f3d5c990d3b
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-05-04 22:31:33 -0400

    committed TODO example

---
+++ linux/nginx_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+context 'Nginx Config Test' do
+  # configuration:
+  # https://github.com/1connect/nginx-config-formatter
+  # https://github.com/vasilevich/nginxbeautifier
+  # log format: https://github.com/pvdb/ntail
+end
