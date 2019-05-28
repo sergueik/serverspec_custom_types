8563b97f9a4cf2549fbc18903fb4c0504e37ec2e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-05-28 17:02:57 -0400

    intermediate revision - execute sensitive puppet command in the spec test

---
+++ linux/puppet_undef_value_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+context 'Puppet type exercise' do
+  describe command( <<-EOF
+    puppet apply -e 'class test(Variant[String, Undef]$value = undef){ if $value { notify {"value \\"${value}\\" is true": }} else { notify {"value \\"${value}\\" is false": }  } } class {"test": }'
+  EOF
+  ) do
+    let(:path) { '/bin:/usr/bin:/sbin:/opt/puppetlabs/puppet/bin'}
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should contain 'value "" is false' }
+    # Use of 'hiera.yaml' version 3 is deprecated. It should be converted to version 5\n
+    # its(:stderr) { should be_empty }
+  end
+  describe command( <<-EOF
+    puppet apply -e 'class test(Variant[String, Undef]$value = undef){ if $value { notify {"value \\"${value}\\" is true": }} else { notify {"value \\"${value}\\" is false": }  } } class {"test":  value => "" }'
+  EOF
+  ) do
+    let(:path) { '/bin:/usr/bin:/sbin:/opt/puppetlabs/puppet/bin'}
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should contain 'value "" is true' }
+    its(:stderr) { should be_empty }
+  end
+end
