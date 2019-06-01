48f7d6c7594a75ba5beecbac4509db7aa3fdddf0
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-06-01 12:59:08 -0400

    intermediate revision - misc fixes of augeas examples

--- linux/augeas_exec_spec_part1.rb
+++ linux/augeas_exec_spec_part1.rb
@@ -3,21 +3,27 @@ require 'spec_helper'
 context 'Augeas Match' do
 
   # default yum install
-  catalina_home = '/usr/share/tomcat'
-  aug_script = '/tmp/example.aug'
-  xml_file = "#{catalina_home}/conf/server.xml"
+  catalina_home = '/opt/tomcat'
+  aug_script = "/tmp/example-#{Process.pid}.au"
+  server_config_file = "#{catalina_home}/conf/server.xml"
 
-  class_name = 'org.apache.catalina.startup.VersionLoggerListener'
-  aug_path = "Server/Listener[#attribute/className=\"#{class_name}\"]/#attribute/className"
-  # the augtool match command would print 
+  class_name = 'org.apache.catalina.core.AprLifecycleListener'
+  aug_path = "Server/Listener[#attribute/className=\"#{class_name}\"]/#attribute/SSLEngine"
+  aug_path_response = '/Server/Listener[2]/#attribute/SSLEngine'
+  # inspecting
+  # <Listener className="org.apache.catalina.core.AprLifecycleListener" SSLEngine="on" />
+  # the augtool match command would print
   # matching nodes in the abbreviated augeas Path notation
+  # that resembles DOM XPath
   # https://github.com/hercules-team/augeas/wiki/Path-expressions#Path_Expressions_by_Example
-  aug_path_response = 'Server/Listener[1]/#attribute/className'
   program=<<-EOF
-    set /augeas/load/xml/lens "Xml.lns"
-    set /augeas/load/xml/incl "#{xml_file}"
+    # NOTE: no magic meaning of 'o' in the /augeas/load path
+    set /augeas/load/o/lens 'Xml.lns'
+    set /augeas/load/o/incl '#{server_config_file}'
+    # NOTE: leading slash optional ?
     load
-    match /files#{xml_file}/#{aug_path} #{class_name}
+    ls /files/opt/tomcat/conf/server.xml/Server/Service[#attribute/name = 'Catalina' ]/
+    match /files#{server_config_file}/#{aug_path} 'on'
   EOF
 
   describe command(<<-EOF
@@ -26,7 +32,7 @@ context 'Augeas Match' do
   EOF
   ) do
     let(:path) { '/bin:/usr/bin:/sbin:/opt/puppetlabs/puppet/bin'}
-    its(:stdout) { should match Regexp.escape( "/files#{xml_file}" + aug_path_response ) }
+    its(:stdout) { should match Regexp.escape( "/files#{server_config_file}" + aug_path_response ) }
     its(:stderr) { should be_empty }
     its(:exit_status) {should eq 0 }
   end
--- linux/augeas_with_puppet_exec_spec.rb
+++ linux/augeas_with_puppet_exec_spec.rb
@@ -1,14 +1,13 @@
 require 'spec_helper'
 
 context 'Augeas Resource Execution' do
-
+  # https://gist.github.com/jniesen/aecf5ee3a3f3bd55c7a84ffe4c432408
   systemd_home = '/usr/lib/systemd/system'
   unit_name = 'mysqld.service'
-  aug_script = '/tmp/example.aug'
   unit_file = "#{systemd_home}/#{unit_name}"
   # NOTE: this is a fragment of a real systemd unit, not for prod use
-  unit_file_cotents = <<-EOF
-  [Unit]
+  unit_file_contents = <<-EOF
+[Unit]
 Description=MySQL Server
 After=network.target
 After=syslog.target
@@ -40,25 +39,35 @@ ExecStart=/usr/sbin/mysqld --daemonize --pid-file=/var/run/mysqld/mysqld.pid $MY
 
   EOF
   lens = 'Systemd.lns'
-
+  aug_script = "/tmp/example-#{Process.pid}.au"
   setting_path = 'Service/PermissionsStartOnly'
+  comment = '\"this is comment\"'
   program=<<-EOF
-    set /augeas/load/Systemd/lens "#{lens}"
-    set /augeas/load/Systemd/incl #{unit_file}
+    set /augeas/load/o/lens '#{lens}'
+    set /augeas/load/o/incl '#{unit_file}'
     load
     set /files#{unit_file}/#{setting_path}/value 'true'
     save
-    ls /files#{unit_file}/#{setting_path}[value= 'true']
+    ls /files#{unit_file}/#{setting_path}
+    # NOTE: error: No match for path expression
+    insert #comment before /files#{unit_file}/#{setting_path}
+    set #comment[last()] '#{comment}'
+    # save
+    # NOTE: Error in /usr/lib/systemd/system/mysqld.service (put_failed)
   EOF
   describe command(<<-EOF
+    # NOTE: echo creates problems with quote preserving
     echo '#{unit_file_contents}' > #{unit_file}
     echo '#{program}' > #{aug_script}
-    augtool A -f #{aug_script}
+    augtool -A -f #{aug_script}
   EOF
   ) do
     let(:path) { '/bin:/usr/bin:/sbin:/opt/puppetlabs/puppet/bin'}
-    its(:stdout) { should match 'dummy' }
+    its(:stdout) { should match Regexp.new(Regexp.escape('Saved 1 file(s)')) }
+    its(:stdout) { should match 'value = true' }
     its(:stderr) { should be_empty }
+    # Error in /usr/lib/systemd/system/mysqld.service:1.0 (parse_skel_failed)  parse can not process entire input
+    # Lens: /usr/share/augeas/lenses/dist/inifile.aug
     its(:exit_status) {should eq 0 }
   end
 end
