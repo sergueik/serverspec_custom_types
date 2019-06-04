e620e8b5f1ec9bab2afca235ad2d49e9f6c59bda
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-06-04 16:50:21 -0400

    intermediate revision - added bolt run spec with script

--- windows/bolt_spec.rb
+++ windows/bolt_spec.rb
@@ -2,27 +2,27 @@ require_relative '../windows_spec_helper'
 
 context 'Bolt' do
 
-# A 32-bit windows msi installer is not easily available for
-# Puppet 6's Bolt
-# https://puppet.com/docs/bolt/latest/bolt_installing.html
-# and Puppet Agent 1.10 's embedded Ruby needs an gem upgrade
-# However it appears easy to installl Bold as gem 
-# inside the uru Ruby 2.3.x environment:
-# The only drawback it will bring together its 55 dependencies 
-# https://www.google.com/search?q=puppet%20bold%20powwershell%20task%20example
+  # A 32-bit windows msi installer is not easily available for
+  # Puppet6's Bolt
+  # https://puppet.com/docs/bolt/latest/bolt_installing.html
+  # and Puppet Agent 1.10 's embedded Ruby needs an gem upgrade
+  # However it appears easy to installl Bold just as gem 
+  # inside the uru Ruby 2.3.x environment:
+  # The only drawback it would lead gem to insall its 55 dependencies together with itself 
 
-$user = ENV.fetch('USERNAME', 'vagrant')
-# on a standalone Windows machine, userdomain will be same as hostname 
-$hostname = ENV.fetch('USERDOMAIN', nil) 
-# $env:PASSWORD='Your AD password'
-$password = ENV.fetch('PASSWORD', 'vagrant')
-[
-  'localhost',
-  $hostname
-].each do |hostname|
+  $user = ENV.fetch('USERNAME', 'vagrant')
+  # on a standalone Windows machine, userdomain will be same as hostname 
+  $hostname = ENV.fetch('USERDOMAIN', nil) 
+  # $env:PASSWORD='Your AD password'
+  $password = ENV.fetch('PASSWORD', 'vagrant')
+  [
+    'localhost',
+    $hostname
+  ].each do |hostname|
     $stderr.puts "ruby c:/uru/ruby/lib/ruby/gems/2.3.0/gems/bolt-1.21.0/exe/bolt command run 'Get-Process' --nodes winrm://#{hostname} --no-ssl --user '#{$user}' --password '#{$password}'"
+    # https://www.google.com/search?q=puppet%20bold%20powwershell%20task%20example
     describe command(<<-EOF
-      ruby ./gems/2.3.0/gems/bolt-1.21.0/exe/bolt command run 'Get-Process' --nodes winrm://#{hostname} --no-ssl --user '#{$user}' --password '#{$password}'
+      ruby ruby/lib/ruby/gems/2.3.0/gems/bolt-1.21.0/exe/bolt command run 'Get-Process' --nodes winrm://#{hostname} --no-ssl --user '#{$user}' --password '#{$password}'
      EOF
     ) do
      [ 
@@ -34,4 +34,115 @@ $password = ENV.fetch('PASSWORD', 'vagrant')
       its(:stderr) { should be_empty }
     end 
   end
+  describe command(<<-EOF
+$script = @'  
+  # origin: http://poshcode.org/5679 for additional registry hack
+
+$schemas = @(
+  'http',
+  'https',
+  'ftp'
+)
+$browsers = @{
+  'FirefoxURL' = 'Firefox';
+  'Opera\\.Protocol' = 'Opera';
+  'ChromeHTML' = 'Chrome';
+  'IE\\..*' = 'Interner Explorer';
+}
+pushd 'HKCU:'
+cd '/Software/Microsoft/Windows/Shell/Associations/UrlAssociations'
+$schemas | ForEach-Object {
+  $schema = $_
+  pushd $schema
+  $x = Get-ItemProperty -Path 'UserChoice' -Name 'Progid'
+  $handler = $x.'Progid'
+  $browsers.Keys | ForEach-Object {
+    if ($handler -match $_) {
+      write-host ('{0} is the default for {1}' -f $browsers[$_], $schema)
+    }
+  }
+  popd
+}
+popd
+'@
+
+    echo $script | out-file -literalpath 'c:/temp/script.ps1'
+    ruby ruby/lib/ruby/gems/2.3.0/gems/bolt-1.21.0/exe/bolt script run c:/temp/script.ps1 --nodes winrm://localhost --no-ssl --user '#{$user}' --password '#{$password}'
+   EOF
+  ) do
+   [ 
+    "Started on localhost...",
+    "Finished on localhost:",
+    'Chrome is the default',
+   ].each do |line|
+      its(:stdout) { should contain line }
+    end
+    its(:stderr) { should be_empty }
+  end 
 end
+
+
+
+# .\uru_rt.exe gem install --no-rdoc --no-ri bolt
+# 
+# ruby 2.3.3p222 (2016-11-21 revision 56859) [i386-mingw32]
+# 
+# public_suffix-3.1.0
+# addressable-2.6.0
+# CFPropertyList-2.3.6
+# concurrent-ruby-1.1.5
+# excon-0.64.0
+# docker-api-1.34.2
+# optimist-3.0.0
+# highline-1.6.21
+# hiera-eyaml-3.0.0
+# little-plugger-1.1.4
+# logging-2.2.2
+# minitar-0.6.1
+# net-ssh-5.2.0
+# multipart-post-2.1.1
+# faraday-0.13.1
+# connection_pool-2.2.2
+# net-http-persistent-3.0.1
+# orchestrator_client-0.4.2
+# ffi-1.9.25-x86-mingw32
+# facter-2.5.1-x86-mingw32
+# hiera-3.5.0
+# semantic_puppet-1.0.2
+# fast_gettext-1.1.2
+# locale-2.1.2
+# httpclient-2.8.3
+# hocon-1.2.5
+# puppet-resource_api-1.8.1
+# win32-dir-0.4.9
+# win32-process-0.7.5
+# win32-security-0.2.5
+# win32-service-0.8.8
+# puppet-6.4.2-x86-mingw32
+# colored-1.2
+# cri-2.15.6
+# log4r-1.1.10
+# faraday_middleware-0.12.2
+# text-1.3.1
+# gettext-3.2.9
+# gettext-setup-0.30
+# puppet_forge-2.2.9
+# r10k-3.3.0
+# rubyntlm-0.6.2
+# windows_error-0.1.2
+# bindata-2.4.4
+# ruby_smb-1.0.5
+# unicode-display_width-1.6.0
+# terminal-table-1.8.0
+# builder-3.2.3
+# erubis-2.7.0
+# gssapi-1.3.0
+# gyoku-1.3.1
+# nori-2.6.0
+# winrm-2.3.2
+# rubyzip-1.2.3
+# winrm-fs-1.3.2
+# bolt-1.21.0
+# 
+# 56 gems installed
+# 
