b5326d3ef4308072cd7023358445122102041b4a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-06-13 00:50:38 -0400

    intermediate revision - refactored setup and valdation of java (*assuming springboot) jackson jar YAML deserializing

--- windows/jackson_spec.rb
+++ windows/jackson_spec.rb
@@ -1,33 +1,55 @@
 require_relative '../windows_spec_helper'
+
 # require 'spec_helper'
 
+require 'fileutils'
+
+$DEBUG = ENV.fetch('DEBUG', 'false')
+$stderr.puts "Literal $DEBUG='#{$DEBUG}'"
+$DEBUG = (ENV.fetch('DEBUG', 'false') =~ /^(true|t|yes|y|1)$/i)
+$stderr.puts "Evaluated $DEBUG='#{$DEBUG ? 'true': 'false'}'"
 
 context 'Jackson YAML', :if => os[:family] == 'windows' do
 
   # based on: https://dzone.com/articles/read-yaml-in-java-with-jackson
   java_version = '1.8.0_101'
   # expect dependency jars to be somehow present in #{target_lib_path}
+  jackson_version = '2.9.9'
+  commons_lang3_version = '3.0.1'
+  log4j_version = '2.5'
+  snakeyaml_version = '1.24'
   jars = [
-    'commons-lang3-3.0.1.jar',
-    'jackson-annotations-2.9.9.jar',
-    'jackson-core-2.9.9.jar',
-    'jackson-databind-2.9.9.jar',
-    'jackson-dataformat-yaml-2.9.9.jar',
-    'log4j-api-2.5.jar',
-    'log4j-core-2.5.jar',
-    'snakeyaml-1.24.jar', # dependency of the jackson jar!
+    "commons-lang3-#{commons_lang3_version}.jar",
+    "jackson-annotations-#{jackson_version}.jar",
+    "jackson-core-#{jackson_version}.jar",
+    "jackson-databind-#{jackson_version}.jar",
+    "jackson-dataformat-yaml-#{jackson_version}.jar",
+    "log4j-api-#{log4j_version}.jar",
+    "log4j-core-#{log4j_version}.jar",
+    "snakeyaml-#{snakeyaml_version}.jar", # dependency of the jackson jar!
   ]
   path_separator = ';'
   target_lib_path = 'target/lib' # using default maven package install command to set up the environment
   jars_cp = jars.collect{|jar| "#{target_lib_path}/#{jar}"}.join(path_separator)
-  database_host = 'localhost'
-  database_name = 'information_schema'
-  username = 'root'
-  password = 'password'
 
-  class_name = 'JacksonExample'
+  homedir = ENV.fetch('USERPROFILE', 'c:/Users/Vagrant')
+  yaml_filename = 'user.yaml'
+  yaml_data = <<-EOF
+---
+name: Test User
+age: 30
+address:
+  line1: My Address Line 1
+  line2: ~
+  city: Washington D.C.
+  zip: 20000
+roles:
+  - User
+  - Editor
+  EOF
+  java_class_name = 'JacksonExample'
 
-  source = <<-EOF
+  java_source = <<-EOF
 
   import java.util.Arrays;
   import java.util.Map;
@@ -42,11 +64,11 @@ context 'Jackson YAML', :if => os[:family] == 'windows' do
 
   import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
   import org.apache.commons.lang3.builder.ToStringStyle;
-    public class #{class_name} {
+    public class #{java_class_name} {
 
     private static boolean debug = false;
 
-    private static final String dataFileName = "user.yaml";
+    private static final String dataFileName = "#{yaml_filename}";
     private static final YAMLFactory yamlFactory = new YAMLFactory();
     static {
       yamlFactory.configure(YAMLGenerator.Feature.USE_NATIVE_TYPE_ID, false)
@@ -114,18 +136,32 @@ context 'Jackson YAML', :if => os[:family] == 'windows' do
     }
   }
   EOF
+  before(:each) do
+    $stderr.puts "Writing #{yaml_filename}"
+    file = File.open("#{homedir}/#{yaml_filename}", 'w')
+    file.puts yaml_data
+    file.close
+    $stderr.puts "Writing #{java_class_name}"
+    file = File.open("#{homedir}/#{java_class_name}.java", 'w')
+    file.puts java_source
+    file.close
+  end
+
   describe command(<<-EOF
-    pushd $env:USERPROFILE
-    write-output '#{source}' | out-file #{class_name}.java -encoding ASCII
+    pushd '#{homedir}'
     $env:PATH = "${env:PATH};c:\\java\\jdk#{java_version}\\bin"
-    # javac -cp target\\lib\\* JacksonExample.java
-    javac -cp target\\lib\\* '#{class_name}.java'
-    # would produce
-    # '#{class_name}$User.class'
-    # '#{class_name}.class'
-    cmd %%- /c "java -cp #{jars_cp}#{path_separator}. #{class_name}"
+    javac -cp -cp #{jars_cp} '#{java_class_name}.java'
+    cmd %%- /c "java -cp #{jars_cp}#{path_separator}. #{java_class_name}"
   EOF
   ) do
+    [
+      "#{homedir}/#{java_class_name}$User.class",
+      "#{homedir}/#{java_class_name}.class"
+    ].each do |class_file|
+      describe file(class_file) do
+        it { should be_file }
+      end
+    end
     its(:exit_status) { should eq 0 }
     [
       'name=Test User',
