5fb52a5c7411bf0f6b612cd9cdda1016959a986a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-06-16 12:34:26 -0400

    intermediate revision - fixed typo, added autologin config spec for allowing vnc on unattended

--- linux/vino_spec.rb
+++ linux/vino_spec.rb
@@ -1,10 +1,27 @@
 require 'spec_helper'
 
 context 'xFCE Session and Startup desktop launchers' do
+  # will it be the correct user?
   user = ENV.fetch('USER')
   launcher_dir = "/home/#{user}/.config/autostart"
+  lightdm_dir = '/usr/share/lightdm/lightdm.conf.d'
   context 'Vino launcher' do
-    # will it be the correct user?
+    # https://askubuntu.com/questions/83824/how-can-i-start-a-vnc-server-before-log-on
+    # https://askubuntu.com/questions/530072/how-to-auto-login-in-xubuntu
+    describe file("#{lightdm_dir}/60-xubuntu.conf") do
+      it { should be_file }
+      [
+        '[Seat:*]',
+      ].each do |section|
+        its(:content) { should contain section }
+      end
+      {
+        'user-session'   => 'xubuntu',
+        'autologin-user' => user
+      }.each do |setting,value|
+        its(:content) { should contain "#{setting}=#{value}" }
+      end
+    end
     describe file("#{launcher_dir}/vino-server.desktop") do
       it { should be_file }
       # type
@@ -13,7 +30,7 @@ context 'xFCE Session and Startup desktop launchers' do
       ].each do |section|
         its(:content) { should contain section }
       end
-      # probaly not all settings required
+      # probaly not every setting is required
       {
         'Encoding'      => 'UTF-8',
         'Version'       => '0.9.4',
@@ -25,8 +42,8 @@ context 'xFCE Session and Startup desktop launchers' do
         'StartupNotify' => 'false',
         'Terminal'      => 'false',
         'Hidden'        => 'false',
-      }.each do |key,val|
-        its(:content) { should contain section }
+      }.each do |setting,value|
+        its(:content) { should contain "#{setting}=#{value}" }
       end
     end
   end
@@ -35,7 +52,7 @@ context 'xFCE Session and Startup desktop launchers' do
     describe file(desktop_launcher_file) do
       it { should be_file }
       # #let or #subject called without a block (RuntimeError)
-      # subject {    $stderr.puts self  }
+      # subject { $stderr.puts self }
       $stderr.puts "'#{self.class.name.split('::').last}'" # 'Class'
       $stderr.puts "'#{self.to_s.split('::').last}'" # 'FileHomeRootConfigAutostartBluemanDesktop'
       its(:content) { should contain '[Desktop Entry]' } # TODO: first line
@@ -45,8 +62,8 @@ context 'xFCE Session and Startup desktop launchers' do
       # Removed from Application Autostart is simply Hidden at the xFCE launcher
       {
         'Hidden' => 'true',
-      }.each do |key,val|
-        its(:content) { should contain section }
+      }.each do |setting,value|
+        its(:content) { should contain "#{setting}=#{value}" }
       end
     end
   end
