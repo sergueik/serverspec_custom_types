f544334bfe923de2d0682a3e0d2b3f2c45f22b57
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-06-20 01:35:24 -0400

    intermediate revision - added YAML into JSON filtering via jackson and one of the available JSON loading tools (not really finished)

--- windows/jackson_spec.rb
+++ windows/jackson_spec.rb
@@ -172,4 +172,141 @@ roles:
       its(:stdout) { should match line}
     end
   end
+  java_class2_name = 'JacksonYAMLExample'
+
+  java_source2 = <<-EOF
+
+  import java.util.Arrays;
+  import java.util.Map;
+  import java.io.File;
+
+  import org.apache.logging.log4j.LogManager;
+  import org.apache.logging.log4j.Logger;
+
+  import com.fasterxml.jackson.databind.ObjectMapper;
+  import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+  import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
+  // for converting into json
+  import com.fasterxml.jackson.databind.SequenceWriter;
+  import com.fasterxml.jackson.core.JsonProcessingException;
+
+  import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+  import org.apache.commons.lang3.builder.ToStringStyle;
+    public class #{java_class_name} {
+
+    private static boolean debug = false;
+
+    private static final String dataFileName = "#{yaml_filename}";
+    private static final YAMLFactory yamlFactory = new YAMLFactory();
+    static {
+      yamlFactory.configure(YAMLGenerator.Feature.USE_NATIVE_TYPE_ID, false)
+          .configure(YAMLGenerator.Feature.MINIMIZE_QUOTES, true)
+          .configure(YAMLGenerator.Feature.ALWAYS_QUOTE_NUMBERS_AS_STRINGS, true);
+    }
+    private static ObjectMapper inputObjectMapper = new ObjectMapper(yamlFactory);
+    private static ObjectMapper ouputObjectMapper = new ObjectMapper();
+    // fallback to JSON
+
+    private static String yamlString = null;
+    private static String jsonString = null;
+
+    private static final Logger log = LogManager.getLogger(JacksonExample.class);
+
+    public static void main(String[] args) { 
+    // converting YAML to JSON: the JSON loading tools are easier to find
+
+      try {
+        User user = inputObjectMapper
+            .readValue(new File(String.join(System.getProperty("file.separator"),
+                Arrays.asList(System.getProperty("user.dir"), dataFileName))),
+                User.class);
+          jsonString = ouputObjectMapper.writeValueAsString(user);
+          System.err.println(
+          String.format("JSON serialization with Jackson: \\n%s\\n", jsonString));
+        // System.out.println(ReflectionToStringBuilder.toString(user,
+        //   ToStringStyle.MULTI_LINE_STYLE));
+      } catch (Exception e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+
+    private static class User {
+      private String name;
+      private int age;
+      private Map<String, String> address;
+      private String[] roles;
+
+      public String getName() {
+        return name;
+      }
+
+      public void setName(String data) {
+        this.name = data;
+      }
+
+      public int getAge() {
+        return age;
+      }
+
+      public void setAge(int data) {
+        this.age = data;
+      }
+
+      public Map<String, String> getAddress() {
+        return address;
+      }
+
+      public void setAddress(Map<String, String> data) {
+        this.address = data;
+      }
+
+      public String[] getRoles() {
+        return roles;
+      }
+
+      public void setRoles(String[] data) {
+        this.roles = data;
+      }
+    }
+  }
+  EOF
+  before(:each) do
+    $stderr.puts "Writing #{yaml_filename}"
+    file = File.open("#{homedir}/#{yaml_filename}", 'w')
+    file.puts yaml_data
+    file.close
+    $stderr.puts "Writing #{java_class_name}"
+    file = File.open("#{homedir}/#{java_class2_name}.java", 'w')
+    file.puts java_source2
+    file.close
+  end
+
+  describe command(<<-EOF
+    pushd '#{homedir}'
+    $env:PATH = "${env:PATH};c:\\java\\jdk#{java_version}\\bin"
+    # javac -cp #{jars_cp} '#{java_class2_name}.java'
+    # temprarily glob
+    javac -cp target/lib/* '#{java_class2_name}.java'
+    cmd %%- /c "java -cp #{jars_cp}#{path_separator}. #{java_class_name}"
+  EOF
+  ) do
+    [
+      "#{homedir}/#{java_class2_name}$User.class",
+      "#{homedir}/#{java_class2_name}.class"
+    ].each do |class_file|
+      describe file(class_file) do
+        it { should be_file }
+      end
+    end
+    its(:exit_status) { should eq 0 }
+    [
+      'name=Test User',
+      'age=30',
+      'address={line1=My Address Line 1, line2=null, city=Washington D.C., zip=20000}',
+      'roles={User,Editor}',
+    ].each do |line|
+      its(:stdout) { should match line}
+    end
+  end
 end
