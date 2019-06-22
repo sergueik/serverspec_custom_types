988915529c802e52aa90dd1501066459fafe59be
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-06-22 01:38:27 -0400

    intermediate revision - fixed shell script

--- linux/artifact_hash_check_spec.rb
+++ linux/artifact_hash_check_spec.rb
@@ -1,41 +1,58 @@
 require 'spec_helper'
 require 'pp'
 
+# expects the Puppet hieradata configuration to match
+# the file hash of artifact of war installed in tomcat container
 context 'Artifact hash check' do
+  config_file = 'common.yaml'
+  config_key = 'artifact_checksum'
+  config_path = '.'
+  artifact='dummy.file'
+  hiera_path_check='hieradata'
+  tomcat_appdir = '/opt/tomcat'
   describe command(<<-EOF
-    CONFIG_PATH='.'
-    CONFIG_FILE='common.yaml'
-    TOMCAT_APPDIR='/opt/tomcat'
+    DEBUG=
+    CONFIG_PATH='#{config_path}'
+    CONFIG_FILE='#{config_file}'
+    CONFIG_KEY='#{config_key}'
+    TOMCAT_APPDIR='#{tomcat_appdir}'
     ARTIFACT_PATH="${TOMCAT_APPDIR}/webapps"
-    # TODO:  read $ARTIFACT from $CONFIG_FILE
-    ARTIFACT='dummy.file'
-    MOUNT_ROOT=$(mount -t vboxsf | grep hieradata | head -1 | cut -f 3 -d ' ')
-    CONFIG_KEY='artifact_checksum'
-    if [[ ! -z $MOUNT_ROOT ]]
+    # TODO: read $ARTIFACT from $CONFIG_FILE
+    ARTIFACT='#{artifact}'
+    HIERA_PATH_CHECK='#{hiera_path_check}'
+    MOUNT_ROOT=$(mount -t vboxsf | grep "${HIERA_PATH_CHECK}" | head -1 | cut -f 3 -d ' ')
+    if [ ! -z $MOUNT_ROOT ]
     then
-      if [[ -d $MOUNT_ROOT ]]
+      if [ -d $MOUNT_ROOT ]
       then
-        echo "Reading \"${CONFIG_KEY}: \" from ${MOUNT_ROOT}/${CONFIG_PATH}/${CONFIG_FILE}"
-        ARTIFACT_HASH=$(grep "${CONFIG_KEY}: " $MOUNT_ROOT/$CONFIG_PATH/$CONFIG_FILE | cut -f 2 -d ':' | sed "s/'//g")
-        echo running  sha256sum "${ARTIFACT_PATH}/${ARTIFACT}"
-
-        sha256sum "${ARTIFACT_PATH}/${ARTIFACT}"
-        ACTUAL_HASH=$(sha256sum "${ARTIFACT_PATH}/${ARTIFACT}" | cut -d ' ' -f 1| sed 's/\n//'| sed 's/ *//g')
-        echo "Comparing ${ACTUAL_HASH} to ${ARTIFACT_HASH}"
-        if [[ "$ACTUAL_HASH" = "$ARTIFACT_HASH" ]]
+        if [ ! -z $DEBUG ] ; then
+          echo "Reading \\"${CONFIG_KEY}: \\" from ${MOUNT_ROOT}/${CONFIG_PATH}/${CONFIG_FILE}"
+        fi
+        NEEDED_HASH=$(grep "${CONFIG_KEY}: " $MOUNT_ROOT/$CONFIG_PATH/$CONFIG_FILE|head -1 | sed "s|${CONFIG_KEY}: ||"|tr -d "'"| tr -d '\\r'| tr -d ' ')
+        if [ ! -z $DEBUG ] ; then
+          echo running  sha256sum "${ARTIFACT_PATH}/${ARTIFACT}"
+          sha256sum "${ARTIFACT_PATH}/${ARTIFACT}"
+        fi
+        ACTUAL_HASH=$(sha256sum "${ARTIFACT_PATH}/${ARTIFACT}" | cut -d ' ' -f 1| tr -d '\\n')
+        if [ ! -z $DEBUG ] ; then
+          echo "Comparing ${ACTUAL_HASH} to ${NEEDED_HASH}"
+        fi
+        if [ "$ACTUAL_HASH" = "$NEEDED_HASH" ]
         then
           echo 'Valid'
         else
           echo 'Invalid'
-          echo "ACTUAL_HASH='$ACTUAL_HASH'   "
-          echo  "ARTIFACT_HASH='$ARTIFACT_HASH'   "
+          if [ ! -z $DEBUG ] ; then
+            echo "ACTUAL_HASH='$ACTUAL_HASH'"
+            echo "NEEDED_HASH='$NEEDED_HASH'"
+          fi
         fi
       fi
     fi
   EOF
   ) do
     its(:exit_status) { should eq 0 }
-    its(:stdout) { should contain 'Valid`' }
+    its(:stdout) { should contain 'Valid' }
     its(:stderr) { should be_empty }
   end
 end
