a74f03fbeeab7bcc7c9be7dcf0e51c0c684392d6
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-06-24 01:00:31 -0400

    inyermediate revision - annotated the spec_helper files

--- staging/windows/spec/windows_spec_helper.rb
+++ staging/windows/spec/windows_spec_helper.rb
@@ -1,12 +1,13 @@
 require 'serverspec'
+# This winsows_spec_helper.rb is for running serverspec locally on a Windows box
 
-# in Windows environment 'spec_helper.rb' is usually present but unused
-# 'windows_spec_helper.rb' is required by individual spec files
-# setting the :backend incorrectly to exec would lead to error
-# Errno::ENOENT:
-#  No such file or directory - /bin/sh -c ls\ /etc/coreos/update.conf
 
 set :backend, :cmd
+# 'spec_helper.rb' is usually present but unused in Windows testing
+# by require 'windows_spec_helper.rb' in each individual spec files
+# setting the :backend incorrectly to :exec would manifest through error
+# Errno::ENOENT:
+# No such file or directory - /bin/sh -c ls\ /etc/coreos/update.conf
 
 RSpec.configure do |config|
   config.filter_gems_from_backtrace 'vagrant', 'vagrant-serverspec'
