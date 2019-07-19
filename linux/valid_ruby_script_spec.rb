6b28e3b3ec9a004e7e6966df988845cab9b666d3
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-07-19 15:39:11 -0400

    intermediate reision - added basic ruby spec to examine generated scripts

---
+++ linux/valid_ruby_script_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+require 'fileutils'
+
+# Puppet-generated cron jobs may run Ruby scripts,
+# ensure the syntax of the generated script is correct
+context 'Ruby Script' do
+  script_path = '/tmp'
+  script_filename = 'a.rb'
+  script = "#{script_path}/#{script_filename}"
+  sample_script_data = <<-EOF
+    require 'json'
+    $stderr.puts 'Done.'
+  EOF
+
+  before(:each) do
+    $stderr.puts "Writing #{script}"
+    file = File.open(script, 'w')
+    file.puts sample_script_data
+    file.close
+  end
+
+  describe file script do
+   it {should be_file }
+   it {should be_mode 755 }  # 0755 => mode?(493)
+  end
+  describe command( <<-EOF
+    ruby -c '#{script}'
+  EOF
+  ) do
+   its(:exit_status) { should eq 0 }
+   its(:stdout) { should contain 'Syntax OK' }
+   its(:stderr) { should be_empty }
+  end
+end
