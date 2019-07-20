c920b55a272965d0d9b4ccf94fc427f6219b234c
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-07-20 00:30:18 -0400

    intermediate revision - exotic legacy example

---
+++ windows/cmd_legacy_spec.rb
@@ -0,0 +1,13 @@
+if File.exists?( 'spec/windows_spec_helper.rb')
+  require_relative '../windows_spec_helper'
+end
+
+#
+# based on https://ss64.com/nt/cd.html
+context 'Undocumented environment variables' do
+  describe command( <<-EOF
+  cmd %%- /c echo %=C:%
+  EOF
+  ) do
+  end
+end
