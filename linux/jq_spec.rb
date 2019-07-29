689f5997d17daadcfb53d559c192152812696420
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-07-29 15:15:05 -0400

    intermediate revision - added spec for jq used as a JSON generator role

--- linux/jq_spec.rb
+++ linux/jq_spec.rb
@@ -11,29 +11,103 @@ context 'jq' do
   end
   context 'querying JSON' do
 
-   datafile = '/tmp/sample.json'
+    datafile = '/tmp/sample.json'
 
-   before(:each) do
-    Specinfra::Runner::run_command( <<-EOF
-      cat <<END>#{datafile}
+    before(:each) do
+      Specinfra::Runner::run_command( <<-EOF
+        cat <<END>#{datafile}
 {
 "code": 401
 }
 END
-  EOF
-  )
-  end
+       EOF
+      )
+    end
 
-    # Tomcat configuration is heavily name-spaced
     describe command(<<-EOF
-
       DATAFILE='#{datafile}'
       jq -r '.code' < ${DATAFILE}
-
     EOF
     ) do
       its(:stdout) { should contain '401' }
       its(:stderr) { should be_empty }
     end
+  end
+  context 'Generating JSON' do
+
+    # https://toster.ru/q/608027
+    # https://jqplay.org/
+    # https://programminghistorian.org/en/lessons/json-and-jq
+    # https://stackoverflow.com/questions/22434290/jq-bash-make-json-array-from-variable
+    # https://stedolan.github.io/jq/manual/
+    datafile = '/tmp/sample.json'
+    value = 'some data'
+    context 'Simple' do
+      before(:each) do
+        Specinfra::Runner::run_command( <<-EOF
+          VAL='#{value}'
+          DATAFILE='#{datafile}'
+          # filling the template
+          jq -n --arg val "$VAL" '{"key": $val}' | tee $DATAFILE
+        EOF
+        )
+      end
+
+      describe command(<<-EOF
+
+        DATAFILE='#{datafile}'
+        jq -r '.key' < ${DATAFILE}
+
+      EOF
+      ) do
+        its(:stdout) { should contain value }
+        its(:stderr) { should be_empty }
+      end
+    end
+    context 'Array Hacks' do
+      before(:each) do
+        Specinfra::Runner::run_command( <<-EOF
+          VALUE="data,more data,#{value},yet more data"
+          DATAFILE='#{datafile}'
+          # filling the array value in the template indirectly
+          jq -Rn --arg val  "$VALUE" '{"key": $val | split(",") }' | tee $DATAFILE
+        EOF
+        )
+      end
+      describe command(<<-EOF
+
+        DATAFILE='#{datafile}'
+        jq -r '.key[2]' < ${DATAFILE}
+
+      EOF
+      ) do
+        its(:stdout) { should contain value }
+        its(:stderr) { should be_empty }
+      end
+      describe command(<<-EOF
+        echo -e "data,more data,#{value},yet more data" | sed 's|,|\\n|g' | jq -R . | jq -s '{"key": .}' |jq '.key[2]'
+      EOF
+      ) do
+        its(:stdout) { should contain value }
+        its(:stderr) { should be_empty }
+      end
+    end
+    
+    describe command(<<-EOF
+      VALUE='{"val":"#{value}"}'; jq -n --argjson val "$VALUE" '{"key": $val }' | jq '.key.val'
+    EOF
+    ) do
+      its(:stdout) { should contain value }
+      its(:stderr) { should be_empty }
+    end
+
+    describe command(<<-EOF
+    VALUE='{"val":["stuff","#{value}","other stuff"]}'; jq -n --argjson val "$VALUE" '{"key": $val }' | jq '.key.val[1]'
+    EOF
+    ) do
+      its(:stdout) { should contain value }
+      its(:stderr) { should be_empty }
+    end
+    
   end
 end
