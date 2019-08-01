e6751ad697d797697c4d9aca747e7b8e8bafa465
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-08-01 14:36:32 -0400

    intermediate revision - added a check for null short-circuited systemd unit file

--- linux/systemd_spec.rb
+++ linux/systemd_spec.rb
@@ -1,19 +1,27 @@
 require 'spec_helper'
 
 context 'Exact Match test' do
-  # alternatively command systemctl to format output in the list format
-  # NOTE:  for AppDynamics and Javaagent see
+  unit = 'httpd'
+  # in some occasions devops have a habit of making the systemd service unit
+  # be short-circuited to a link to /dev/null.
+  # Ensure this is not the case
+  describe file "/etc/systemd/system/#{unit}.service" do
+    it { should_not be_symlink }
+    it { should_not be_linked_to('/dev/null') }
+  end	
+  # instruct systemctl to format ithe output as list
+  # NOTE: for AppDynamics and Javaagent see
   # https://zeroturnaround.com/rebellabs/how-to-inspect-classes-in-your-jvm/
   # https://github.com/zeroturnaround/callspy/
   # https://docs.appdynamics.com/
   # AppDynmics simply suggests make its javaagent jar the instrumentation agent
   # that utilizes the Instrumentation API that the JVM provides
-  # via pretty simple means, by executing the javaAgent class premain method before 
+  # via pretty simple means, by executing the javaAgent class premain method before
   # the main method of the subject application class
   # and allowing "java.lang.instrument.Instrumentation" instrument the bytecode of the application
   # to allow appDynamics do its wonders to the available JVM instance:
   # JAVA_OPTS=...-javaagent:/home/appdynamics/AppServerAgent/javaagent.jar...
-  # this demistifies the mehanism behind what is offered by AppDynamics as "information points" 
+  # this demistifies the mehanism behind what is offered by AppDynamics as "information points"
   # metrics of the particular  target appliction method invocation
   info = {
     'Description' => 'The Apache HTTP Server',
@@ -26,7 +34,8 @@ context 'Exact Match test' do
     'ExecStop'    => '{ path=/bin/kill ; argv[]=/bin/kill -WINCH ${MAINPID} ; ignore_errors=no ; start_time=[n/a] ; stop_time=[n/a] ; pid=0 ; code=(null) ; status=0/0 }',
   }
   info_regexp = '(:?' + info.keys.join('|') + ')'
-  command = "systemctl --no-page show httpd | grep -E '#{info_regexp}=' "
+  command = "systemctl --no-page show #{unit} | grep -E '#{info_regexp}=' "
+  # exercise the command
   service_info = command(command).stdout
   $stderr.puts "inspecting:\n------\n#{service_info}\n------\n"
   info.each do |key,val|
