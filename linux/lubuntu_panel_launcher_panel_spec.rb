278e0ef27431287b7097fc47a8cff3a537354048
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-08-27 11:23:05 -0400

    intermediate revision - added details of the desktop launcher file structure

--- linux/lubuntu_panel_launcher_panel_spec.rb
+++ linux/lubuntu_panel_launcher_panel_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 #
 # NOTE: window manager-specific
 # origin: http://www.cyberforum.ru/shell/thread2492044.html
+# see also :https://unix.stackexchange.com/questions/177386/how-can-i-add-applications-to-the-lxpanel-application-launch-bar-via-cli
 context 'Lubuntu LXDE Launchers' do
 
   [
@@ -13,7 +14,24 @@ context 'Lubuntu LXDE Launchers' do
       it { should be_owned_by 'root' }
       it { should be_mode '644' }
       # may need augeas lense to legitimately parse
+      [
+        '[Desktop Entry]'
+        'Version=[\d.]+',
+        'Name=',
+        'GenericName=Web Browser',
+        'Exec=/usr/bin/vivaldi-stable %U',
+        'StartupNotify=(?:true|false)',
+        'Terminal=false',
+        'Icon=vivaldi',
+        'Type=Application',
+      #  'Actions=new-window;new-private-window;'
+      ].each do |line|
+        its(:content) { should match "^\s*" + Regexp.escape(line) }
+      end
     end
+  end
+  describe file "#{env.fetch('HOME')}/.local/share/applications/" do
+    it { should be_directory }
   end
   describe file "#{env.fetch('HOME')}/.config/lxpanel/Lubuntu/panels/panel" do
     it { should be_file }
