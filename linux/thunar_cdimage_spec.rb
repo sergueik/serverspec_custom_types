03a029c34322c8f314b80176b3c167e7391eed92
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-10-14 03:30:07 -0400

    cosmetic refactoring only

--- linux/thunar_cdimage_spec.rb
+++ linux/thunar_cdimage_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
-# The .iso mount/unmount is not provided by default on XFCE
+# The .iso mount/unmount is not provided by default ( specific to XFCE)
+# The PCManFM does not have this problem
 # https://forum.manjaro.org/t/thunar-iso-mount-unmount-is-missing/20168/13
 context 'Disk Image Mounter' do
   context 'packages' do
@@ -9,7 +10,7 @@ context 'Disk Image Mounter' do
     end
   end
   describe file "#{ENV['HOME']}/.config/mimeapps.list" do
-    it { should be_file }
+    it { should be_file } # created on first use
     its(:content) {should match Regexp.new('application/x-cd-image=([a-z0-9-]+.desktop;)*gnome-disk-image-mounter.desktop;') }
   end
 end
