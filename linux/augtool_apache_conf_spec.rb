d70592ba7440d306ff0fcfe2aba7b9c252260458
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-10-18 13:36:05 -0400

    intermediate revision - added a throwaway Puppet code example

--- linux/augtool_apache_conf_spec.rb
+++ linux/augtool_apache_conf_spec.rb
@@ -20,5 +20,33 @@ context 'Augeas' do
       its(:stderr) { should be_empty }
       its(:exit_status) {should eq 0 }
   end
+  context 'Use Puppet Exec Provider against Apache configuration' do
+    apache_home = '/etc/httpd'
+    node_server_name = 'node-server-name'
+    describe command(<<-EOF
+
+SCRIPT=$(cat <<EOF)
+\\$conf_files = ['mime.conf']
+\\$conf_files.each |String |\\$conf_file| {
+  \\$file_path = \\${apache_home}/etc/httpd/conf.d/${cond_file}"
+  exec {"modify \\${conf_file}":
+    command   => "sed -i 's|^\\\\([^#]\\\\)|# \\\\1|g' \\${file_path}",
+    logoutput => true,
+      path      => '/bin:/usr/bin/',
+    onlyif    => "grep -iq '^[^#]' \\${file_path}",
+  }
+}
+      puppet apply -e "$SCRIPT"
+
+    EOF
+    ) do
+      # NOTE: using Puppet apply is a bad idea,      # since it leads to corrupting the configuration when no match found
+      let(:path) { '/bin:/usr/bin:/sbin:/opt/puppetlabs/bin'}
+      its(:stdout) { should match /Notice: Applied catalog/ }
+      its(:stderr) { should be_empty }
+      its(:exit_status) {should eq 0 }
+    end
+  end
+
 end
 
