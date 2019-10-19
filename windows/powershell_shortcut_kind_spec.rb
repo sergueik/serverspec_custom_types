2206c8d6ffad669d47a8edadefba605f908148c7
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-10-19 04:21:37 -0400

    intermediate revision - frequent shortcurt detail detection

---
+++ windows/powershell_shortcut_kind_spec.rb
@@ -0,0 +1,39 @@
+require_relative '../windows_spec_helper'
+
+# based on: https://toster.ru/q/676382
+context 'Shortcuts' do
+  link_name = 'some powershell command'
+  kinds =
+  {
+    'background' => [
+      '-NoLogo',
+      '-NonInteractive',
+      '-NoProfile',
+      '-WindowStyle Hidden',
+      '-ExecutionPolicy Bypass'
+    ],
+    'visible' => [
+      '-NoExit',
+      '-ExecutionPolicy Bypass'
+    ],
+  }
+  # Despite API name the CreateShortcut method is not meant to create the lnk file
+  # but rather deserialzies the shortcut file as an object.
+  describe command(<<-EOF
+  
+    $link_fullpath = "$HOME\\Desktop\\#{link_name}.lnk"
+    $obj_shell = New-Object -ComObject WScript.Shell
+    $obj_link = $obj_shell.CreateShortcut($link_fullpath.FullName)
+    write-output $obj_link.arguments
+  EOF
+  ) do
+    kinds.each do |kind,args|
+      args.each do |arg|
+        its(:stdout) { should match Regexp.new($arg,Regexp::IGNORECASE) }
+      end
+    end
+  end
+end
+
+
+end
