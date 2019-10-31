83c171a033fbde5bd3b6971c9daec413c9f88a07
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-10-31 02:20:34 +0100

    intermediate revision - added attribute clean check spec

---
+++ linux/tomcat_jdbc_config_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+context 'Tomcat server.xml test' do
+  catalina_home = '/usr/share/tomcat'
+  catalina_home =  '/tmp'
+  driver_class_name = 'oracle.jdbc.OracleDriver'
+  resource_name_fragment = 'jdbc'
+  context 'JDBC resource attributes' do
+    # only collects attributes of jdbc resource
+    describe command(<<-EOF
+      xmllint -xpath '//Resource[contains(@name,"#{resource_name_fragment}")]/@*' '#{catalina_home}/conf/server.xml' | tr ' ' '\\n'       
+    EOF
+    ) do      
+      [
+        'type="javax.sql.DataSource"',
+        'auth="Container"',
+        'name="Database"',
+        "driverClassName=\"#{driver_class_name}\"",
+      ].each do |line|
+        its(:stdout) { is_expected.to match Regexp.escape(line) }
+        its(:stderr) { is_expected.to be_empty }
+        # the expect syntax does not support operator matchers, so you must pass a matcher to `#to`.
+        # its(:stderr) { is_expected.to not( contain 'failed to load external entity' )}
+        # undefined local variable or method `is_not_expected' 
+        # its(:stderr) { is_not_expected.to contain 'failed to load external entity' }
+        its(:stderr) { should_not contain 'failed to load external entity' }
+        its(:exit_status) { is_expected.to eq 0 }
+      end
+    end
+  end
+end
+
