30073321c61af4b6821873f90340575677aa8de9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-10-31 04:15:12 +0100

    intermediate revision - fixed the command, removed actually unnecessary context scope

--- linux/sxs_cleanup_inspector_spec.rb
+++ linux/sxs_cleanup_inspector_spec.rb
@@ -6,7 +6,6 @@ context 'Application with past release polluted "side by side" directories' do
   # '/opt/appname/#{past_release}'
   # skipping '/opt/appname/2.4-current' '/opt/appname/current' etc.
   past_releases = %w|1.2.0 1.2.1 1.2.2 1.2.3 1.2.4|
-  # NOTE: needs to have context to follow or does not ever run
   before(:all) do
     # $stderr.puts "Past release dirs: #{past_releases}"
     Specinfra::Runner::run_command( <<-EOF
@@ -17,17 +16,19 @@ context 'Application with past release polluted "side by side" directories' do
       # pushd needs bash
       cd $APP_BASE
       for D in $PAST_RELEASES ; do 1>&2 echo $D; mkdir $D; done
+      mkdir 'dummy'
+      ln -s '#{past_releases[0]}' latest
     EOF
     )
   end
-  context 'Counting sibling application directories' do
-    describe command(<<-EOF
-      ls -l '#{app_base}' | grep -E '[0-9].[0-9].[0-9]+$' | wc -l
-    EOF
-    ) do
-      its(:exit_status) { should eq 0 }
-      its(:stdout) { should match /#{past_releases.size}\b/ }
-    end
+  # counting sibling application directories
+  # NOTE: filtering the directories twice
+  describe command(<<-EOF
+    ls -dl #{app_base}/1* | grep -E '[0-9].[0-9].[0-9]+$' | wc -l
+  EOF
+  ) do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match /#{past_releases.size}\b/ }
   end
   # TODO: Ruby version
 end
