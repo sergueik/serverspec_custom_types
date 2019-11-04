8b907289553436d4c80eb9701fa03551bfa69665
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-11-03 19:52:02 -0500

    intermediate revision - added work example from s.o.

--- linux/tumbler_spec.rb
+++ linux/tumbler_spec.rb
@@ -3,14 +3,20 @@ require 'spec_helper'
 # NOTE: removing tumbler package under Xubuntu likely lead to removal of xubuntu-desktop metapackage - varies with Ubuntu release
 context 'Tumbler Configuration' do
 
-  rc_file = '/etc/xdg/tumbler/tumbler.rc' 
+  rc_file = '/etc/xdg/tumbler/tumbler.rc'
   describe file rc_file do
-    its(:content) { should_not match /Disabled=false/ }   
+    its(:content) { should_not match /Disabled=false/ }
   end
   describe command( <<-EOF
     grep -A4 '\[OdfThumbnailer\]' #{rc_file} | grep Disabled
   EOF
   ) do
-    its(:stdout) { should_not contain /Disabled=false/ }   
+
+    its(:stdout) { should_not contain /Disabled=false/ }
   end
+  # see also: https://github.com/pixelb/crudini/blob/master/crudini
+  # see also: https://stackoverflow.com/questions/6318809/how-do-i-grab-an-ini-value-within-a-shell-script
+  #
+  # sed -nr "/^\[JPEGThumbnailer\]/ { :l /^Disabled[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" /etc/xdg/tumbler/tumbler.rc
+  # true
 end
