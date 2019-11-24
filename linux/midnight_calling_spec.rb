a9a8ae6b9c5e292378c683055dd7c3be2ce27bdf
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-11-24 15:55:06 -0500

    misc. cosmetic fixes and alt strip

--- linux/midnight_calling_spec.rb
+++ linux/midnight_calling_spec.rb
@@ -20,4 +20,20 @@ context 'Mysql Query midnight calculations' do
   ), Specinfra::Runner::run_command("ps ax | grep mysq[l]").exit_status.eql?(0) do
     its(:stdout) { should contain /\d+\-\d+\-\d+\s+\d+:\d+:\d+/ }
   end
+  # TODO: STR_TO_DATE example needed
+  # https://dev.mysql.com/doc/refman/5.6/en/date-and-time-functions.html#function_str-to-date
+  query = <<-EOF
+    SELECT
+      @date_start := CAST(MIN(`EVENT_DATEDIME`) AS DATE),
+      @date_end := CAST(DATE_ADD(MAX(`EVENT_DATEDIME`), INTERVAL 1 DAY) AS DATE)
+    FROM
+      events;
+  EOF
+  describe command(<<-EOF
+    mysql -D '#{db}' -u #{mysql_user} -e "#{query.gsub(/\r?\n/, '')}"
+  EOF
+  ) do
+    its(:stdout) { should contain /\d+\-\d+\-\d+\s+\d+:\d+:\d+/ }
+  end
 end
+
