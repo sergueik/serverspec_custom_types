6ae3c875260f998e8aa387b250623f40f882e7f4
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-11-29 16:59:24 -0500

    intermediate revision - added funny error invalid MySQL attempt to substract dates VB-style error spec

---
+++ linux/mysql_timediff_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+require 'date'
+
+context 'MySQL' do
+
+  context 'Timediff' do
+    start_date = '2019-10-23 10:59:32.000'
+    end_date = '2019-10-23 11:00:29.000'
+
+    sql = <<-EOF
+      use test;
+      SET @start_date = CAST('#{start_date}' AS DATETIME);
+      SET @end_date = CAST('#{end_date}' AS DATETIME);
+      DROP TABLE IF EXISTS example;
+      CREATE TABLE IF NOT EXISTS example (
+        start_date DATETIME NOT NULL,
+        end_date DATETIME NOT NULL
+      );
+      INSERT INTO example (start_date, end_date) values (@start_date, @end_date);
+      SELECT start_date - end_date AS incorrect_diration FROM example;
+      SELECT TIME_TO_SEC(TIMEDIFF(end_date, start_date)) AS correct_duration FROM example;
+    EOF
+    sql.gsub!(/$/, ' ').gsub!(/  +/, ' ')
+    describe command(<<-EOF
+      mysql --silent -e "#{sql}"
+    EOF
+    ) do
+      # in Ruby attempt to substract also appears erroneous
+      $stderr.puts (DateTime.parse(end_date) - DateTime.parse(start_date))
+      duration = (DateTime.parse(end_date).strftime('%s').to_i - DateTime.        parse(start_date).strftime('%s').to_i)
+      its(:stdout) { should contain -4097 } # no idea how MySQL does this
+      its(:stdout) { should contain 57 }
+      its(:stdout) { should contain duration }
+
+    end
+  end
+end
