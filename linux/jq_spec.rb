46f2712359fe40012efc66e8a0ea82f26cf32bb9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-02-25 15:49:50 +0100

    intermediate revision - collapsed and improved data to JSON example

--- linux/jq_spec.rb
+++ linux/jq_spec.rb
@@ -98,8 +98,8 @@ END
     end
 
     describe command(<<-EOF
-      DATAFILE='#{datafile}'
-      jq -r '.code' < ${DATAFILE}
+      DATA_FILE='#{datafile}'
+      jq -r '.code' < ${DATA_FILE}
     EOF
     ) do
       its(:stdout) { should contain '401' }
@@ -119,17 +119,17 @@ END
       before(:each) do
         Specinfra::Runner::run_command( <<-EOF
           VAL='#{value}'
-          DATAFILE='#{datafile}'
+          DATA_FILE='#{datafile}'
           # filling the template
-          jq -n --arg val "$VAL" '{"key": $val}' | tee $DATAFILE
+          jq -n --arg val "$VAL" '{"key": $val}' | tee $DATA_FILE
         EOF
         )
       end
 
       describe command(<<-EOF
 
-        DATAFILE='#{datafile}'
-        jq -r '.key' < ${DATAFILE}
+        DATA_FILE='#{datafile}'
+        jq -r '.key' < ${DATA_FILE}
 
       EOF
       ) do
@@ -141,16 +141,16 @@ END
       before(:each) do
         Specinfra::Runner::run_command( <<-EOF
           VALUE="data,more data,#{value},yet more data"
-          DATAFILE='#{datafile}'
+          DATA_FILE='#{datafile}'
           # filling the array value in the template indirectly
-          jq -Rn --arg val  "$VALUE" '{"key": $val | split(",") }' | tee $DATAFILE
+          jq -Rn --arg val  "$VALUE" '{"key": $val | split(",") }' | tee $DATA_FILE
         EOF
         )
       end
       describe command(<<-EOF
 
-        DATAFILE='#{datafile}'
-        jq -r '.key[2]' < ${DATAFILE}
+        DATA_FILE='#{datafile}'
+        jq -r '.key[2]' < ${DATA_FILE}
 
       EOF
       ) do
@@ -181,6 +181,97 @@ END
       its(:stdout) { should contain value }
       its(:stderr) { should be_empty }
     end
+  end
+  context 'Converting metric sets into JSON' do
+    context 'Temp script' do
+      # NOTE: do not use | as a separator
+      shell_script = '/tmp/a.sh'
+      shell_script_content = <<-EOF
+        #!/bin/bash
+        DATA_FILE1="/tmp/data1.$$"
+        # NOTE: no leading whitespace
+        cat<<DATA>$DATA_FILE1
+first:11:second:12:third:13
+first:22:second:22:third:23
+first:32:second:32:third:33
+DATA
+        DATA_FILE2="/tmp/data2.$$"
 
+        IFS=':'; cat $DATA_FILE1| while read KEY1 VALUE1 KEY2 VALUE2 KEY3 VALUE3; do
+        jq --arg k1 "$KEY1"   \\
+           --arg v1 "$VALUE1" \\
+           --arg k2 "$KEY2"   \\
+           --arg v2 "$VALUE2" \\
+           --arg k3 "$KEY3"   \\
+           --arg v3 "$VALUE3" \\
+           '. | .[$k1]=$v1 | .[$k2]=$v2 | .[$k3]=$v3'  \\
+           <<<'{}' ;
+        done > $DATA_FILE2
+
+        cat $DATA_FILE2 | jq --slurp '.'
+        rm -f $DATA_FILE1
+        rm -f $DATA_FILE2
+      EOF
+      before(:each) do
+        $stderr.puts "Writing #{shell_script}"
+        file = File.open(shell_script, 'w')
+        file.puts shell_script_content.strip
+        file.close
+        File.chmod(0755, shell_script)
+      end
+      # /bin/sh: error while loading shared libraries: libc.so.6: cannot open shared object file: Error 24\
+      describe command(<<-EOF
+        /bin/bash #{shell_script} | tee /tmp/a.log
+      EOF
+      ) do
+        its(:stdout) { should match Regexp.new('"first": "11"') }
+        its(:stderr) { should be_empty }
+      end
+    end
+    context 'Inline script' do
+      # NOTE: when run a inline script the line
+      # <<<'{}'
+      # leads to
+      # 8: Syntax error: redirection unexpected
+      describe command(<<-EOF
+        #!/bin/bash
+        DATA_FILE1="/tmp/data1.$$"
+        # NOTE: heredoc should have no leading whitespace
+        cat<<DATA>$DATA_FILE1
+first:11:second:12:third:13
+first:21:second:22:third:23
+first:31:second:32:third:33
+DATA
+        DATA_FILE2="/tmp/data2.$$"
+
+        IFS=':'
+        cat $DATA_FILE1 | \\
+        while read KEY1 VALUE1 KEY2 VALUE2 KEY3 VALUE3
+        do
+          echo '{}' | \\
+          jq --arg k1 "$KEY1" --arg v1 "$VALUE1" --arg k2 "$KEY2" --arg v2 "$VALUE2" --arg k3 "$KEY3" --arg v3 "$VALUE3" \\
+          '. | .[$k1]=$v1 | .[$k2]=$v2 | .[$k3]=$v3'
+        done > $DATA_FILE2
+        cat $DATA_FILE2 | jq --slurp '.'
+        RESULT_FILE="/tmp/data.$$.json"
+        DATA_KEY='data'
+        # making the rowset keyed by $DATA_KEY
+        # TODO: explore alternatives
+        cat $DATA_FILE2 | jq --slurp '.' | jq "{\\"$DATA_KEY\\": .}" | tee $RESULT_FILE
+        # NOTE: passing key name as argument in the following does not work
+        # jq --arg data_key "$DATA_KEY" '{ .[$data_key] =. }'
+
+        jq '.data|.[]|.first' $RESULT_FILE
+        rm -f $DATA_FILE1
+        rm -f $DATA_FILE2
+        rm -f $RESULT_FILE
+      EOF
+      ) do
+        %w|11 21 31|.each do |value|
+          its(:stdout) { should match Regexp.new("\"#{value}\"") }
+        end
+        its(:stderr) { should be_empty }
+      end
+    end
   end
 end
