2d3067b8ea1bd8ad4392340c83408aa340228a72
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-03-13 18:58:33 -0400

    intermediate revision- advancing example to pass in parameter.and the other with ruby passing to puppet and puppet to also pass one down to shell. note - highly fragile

--- linux/puppet_run_spec.rb
+++ linux/puppet_run_spec.rb
@@ -4,57 +4,57 @@ context 'Examine result of Puppet apply resource run' do
 
   base_dir = '/tmp'
   list_of_dirs = %w|dir1 dir2|
-  
-  before(:each) do
-    ruby_script = <<-EOF
-      require 'fileutils'
-      list_of_dirs = #{list_of_dirs}
-      base_dir = "#{base_dir}"
-      list_of_dirs.each do |dirname|
-        dirpath = "\#{base_dir}/\#{dirname}"          
-        FileUtils.mkdir_p(dirpath) unless File.exists?(dirpath)
-      end
-    EOF
-    Specinfra::Runner::run_command( <<-EOF
-      > /tmp/ruby_script.rb echo '#{ruby_script}'
-      ruby -e '#{ruby_script}'
-    EOF
-    )
-  end
-  target_dir = "#{base_dir}/#{list_of_dirs[0]}"
-  puppet_manifest = <<-EOF
-    exec {'command with condiion':
-      command  => 'echo Done',
-      provider => shell,
-      logoutput => true,
-      onlyif   => 'test -e "#{target_dir}" && ! test -L "#{target_dir}"',
-    }
-  EOF
-  # NOTE: To be able run Puppet in clean ruby-less uru enviroment, 
-  # needed to install running one time from within uru:
-  # uru gem install nokogiri
-  describe command( <<-EOF
-    puppet apply -e "#{puppet_manifest.gsub(/\n/, '').gsub(/\s+/, ' ')}"
-    find #{base_dir} -maxdepth 1 -type d
-  EOF
-) do
-    its(:stderr) { should be_empty }
-    # to make rspec show the actual output, make the expectation unrealistic
-    its(:stdout) { should include 'Done' }
-    its(:stdout) { should include 'Exec[command with condiion]/returns: Done' }
-    its(:exit_status) {should eq 0 }
-  end
-  context 'test 2' do
-    base_dir = '/tmp'
-    list_of_dirs = %w|dir1 dir2|
-    
+  context 'test 1' do
+
     before(:each) do
       ruby_script = <<-EOF
         require 'fileutils'
         list_of_dirs = #{list_of_dirs}
         base_dir = "#{base_dir}"
         list_of_dirs.each do |dirname|
-          dirpath = "\#{base_dir}/\#{dirname}"          
+          dirpath = "\#{base_dir}/\#{dirname}"
+          FileUtils.mkdir_p(dirpath) unless File.exists?(dirpath)
+        end
+      EOF
+      Specinfra::Runner::run_command( <<-EOF
+        > /tmp/ruby_script.rb echo '#{ruby_script}'
+        ruby -e '#{ruby_script}'
+      EOF
+      )
+    end
+    target_dir = "#{base_dir}/#{list_of_dirs[0]}"
+    puppet_manifest = <<-EOF
+      exec {'command with condiion':
+        command  => 'echo Done',
+        provider => shell,
+        logoutput => true,
+        onlyif   => 'test -e "#{target_dir}" && ! test -L "#{target_dir}"',
+      }
+    EOF
+    # NOTE: To be able run Puppet in clean ruby-less uru enviroment,
+    # needed to install running one time from within uru:
+    # uru gem install nokogiri
+    describe command( <<-EOF
+      puppet apply -e "#{puppet_manifest.gsub(/\n/, '').gsub(/\s+/, ' ')}"
+      find #{base_dir} -maxdepth 1 -type d
+    EOF
+  ) do
+      its(:stderr) { should be_empty }
+      # to make rspec show the actual output, make the expectation unrealistic
+      its(:stdout) { should include 'Done' }
+      its(:stdout) { should include 'Exec[command with condiion]/returns: Done' }
+      its(:exit_status) {should eq 0 }
+    end
+  end
+  context 'test 2' do
+
+    before(:each) do
+      ruby_script = <<-EOF
+        require 'fileutils'
+        list_of_dirs = #{list_of_dirs}
+        base_dir = "#{base_dir}"
+        list_of_dirs.each do |dirname|
+          dirpath = "\#{base_dir}/\#{dirname}"
           FileUtils.mkdir_p(dirpath) unless File.exists?(dirpath)
         end
       EOF
@@ -70,17 +70,12 @@ context 'Examine result of Puppet apply resource run' do
         command  => 'echo Done',
         provider => shell,
         logoutput => true,
-        onlyif   => \\"test -e \\${target_dir} && ! test -L \\${target_dir}\\",
+        onlyif   => \\"TARGET_DIR=\\${target_dir};test -e \\\\\\${TARGET_DIR} && ! test -L \\\\\\${TARGET_DIR}\\",
       }
     EOF
-    # NOTE:  over escaping will turn a success int failure
-    #    onlyif   => \\"test -e \\\\\\${target_dir} && ! test -L \\\\\\${target_dir}\\",
-    # NOTE: To be able run Puppet in clean ruby-less uru environment, 
-    # needed to install running one time from within uru:
-    # uru gem install nokogiri
     describe command( <<-EOF
       puppet apply -e "#{puppet_manifest.gsub(/\n/, '').gsub(/\s+/, ' ')}"
-      # 
+      #
       # find #{base_dir} -maxdepth 1 -type d
     EOF
   ) do
@@ -97,5 +92,48 @@ context 'Examine result of Puppet apply resource run' do
     # puppet apply -e "\$target_dir = '/tmp/dir1'; exec {\"command with condition for \${target_dir}\": command => 'echo Done', provider => shell, logoutput => true, onlyif => 'test -e \"\${target_dir}\" && ! test -L \"/tmp/dir1\"', }"
     # puppet apply -e "\$target_dir = '/tmp/dir1'; exec {\"command with condition for \${target_dir}\": command => 'echo Done', provider => shell, logoutput => true, onlyif => 'test -e \"\${target_dir}\" && ! test -L \"\${target_dir}\"', }"
     # puppet apply -e "\$target_dir = '/tmp/dir1'; exec {\"command with condition for \${target_dir}\": command => 'echo Done', provider => shell, logoutput => true, onlyif => \"test -e \\\"\${target_dir}\\\" && ! test -L \\\"\${target_dir}\\\"\", }"
-  end  
+  end
+  context 'test 3' do
+
+    before(:each) do
+      ruby_script = <<-EOF
+        require 'fileutils'
+        list_of_dirs = #{list_of_dirs}
+        base_dir = "#{base_dir}"
+        list_of_dirs.each do |dirname|
+          dirpath = "\#{base_dir}/\#{dirname}"
+          FileUtils.mkdir_p(dirpath) unless File.exists?(dirpath)
+        end
+      EOF
+      Specinfra::Runner::run_command( <<-EOF
+        > /tmp/ruby_script.rb echo '#{ruby_script}'
+        ruby -e '#{ruby_script}'
+      EOF
+      )
+    end
+    target_dir = "#{base_dir}/#{list_of_dirs[0]}"
+    puppet_manifest = <<-EOF
+      exec {'command with condiion':
+        command  => 'echo Done',
+        provider => shell,
+        logoutput => true,
+        onlyif   => 'test -e "#{target_dir}" && ! test -L "#{target_dir}"',
+      }
+    EOF
+    # NOTE: To be able run Puppet in clean ruby-less uru enviroment,
+    # needed to install running one time from within uru:
+    # uru gem install nokogiri
+    describe command( <<-EOF
+      puppet apply -e "#{puppet_manifest.gsub(/\n/, '').gsub(/\s+/, ' ')}"
+      find #{base_dir} -maxdepth 1 -type d
+    EOF
+  ) do
+      its(:stderr) { should be_empty }
+      # to make rspec show the actual output, make the expectation unrealistic
+      its(:stdout) { should include 'Done' }
+      its(:stdout) { should include 'Exec[command with condiion]/returns: Done' }
+      its(:exit_status) {should eq 0 }
+    end
+  end
 end
+
