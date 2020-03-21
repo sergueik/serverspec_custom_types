2722436710783c57ec0c70bca94f930533f9aa30
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-03-21 02:15:33 -0400

    intermediare revision - litle-known set handling example

---
+++ linux/grep_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+require 'fileutils'
+
+# based on: https://www.cyberforum.ru/shell/thread2600637.html
+context 'chained expression grep' do
+  folder = '/tmp'
+  data_file = "#{folder}/a.txt"
+  data = <<-EOF
+    foo something
+      something bar
+        something else
+  EOF
+  before(:each) do
+    $stderr.puts "Writing #{data_file}"
+    file = File.open(data_file, 'w')
+    file.puts (data.gsub(Regexp.new('^\s *',Regexp::MULTILINE), ''))
+    file.close
+  end
+  describe command(<<-EOF
+    grep -e '^foo ' -e ' bar$' '#{data_file}'
+  EOF
+  ) do
+    its(:exit_status) { should eq 0 }
+    [
+      'foo something',
+      'something bar'
+    ].each do |line|
+      its(:stdout) { should match Regexp.new(line, Regexp::IGNORECASE) }
+    end
+    its(:stdout) { should_not match Regexp.new('something else', Regexp::IGNORECASE) }
+    its(:stderr) { should be_empty }
+    its(:exit_status) { should eq 0 }
+  end
+  describe command(<<-EOF
+    tee /tmp/patt.txt <<DATA
+bar$
+^foo
+DATA
+egrep -f /tmp/patt.txt '#{data_file}'
+   EOF
+  ) do
+    its(:exit_status) { should eq 0 }
+    [
+      'foo something',
+      'something bar'
+    ].each do |line|
+      its(:stdout) { should match Regexp.new(line, Regexp::IGNORECASE) }
+    end
+    its(:stdout) { should_not match Regexp.new('something else', Regexp::IGNORECASE) }
+    its(:stderr) { should be_empty }
+    its(:exit_status) { should eq 0 }
+  end
+end
