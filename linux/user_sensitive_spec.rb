156a97bc880f60b16f33e7c426c19d57aff51130
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-03-21 10:19:36 -0400

    intermediate revision - user-sensitive spec variations for practicing the semantics

---
+++ linux/user_sensitive_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+# https://www.ruby-forum.com/t/get-current-user-logged-in-to-local-computer/136885/2
+
+context 'user sensitive' do
+  root_home = '/root'
+  # condition at the 'describe' level
+  context 'home directory' do
+    describe command('echo $HOME'), :if => ENV.fetch('USER').eql?('root') do
+      its(:stdout) { should match Regexp.new(root_home) }
+    end
+    describe command('echo $HOME'), :unless => ENV.fetch('USER').eql?('root') do
+      its(:stdout) { should_not match Regexp.new(root_home) }
+    end
+  end
+  # condition at the 'context' level
+  context 'home directory', :if => ENV.fetch('USER').eql?('root') do
+    describe command('echo $HOME') do
+      its(:stdout) { should match Regexp.new(root_home) }
+    end
+  end
+  context 'home directory', :unless => ENV.fetch('USER').eql?('root') do
+    describe command('echo $HOME') do
+      its(:stdout) { should_not match Regexp.new(root_home) }
+    end
+  end
+  # include branch condition in the 'title' property
+  context "home directory of #{ENV.fetch('USER')}" do
+    describe command('echo $HOME') do
+      its(:stdout) { should_not be_empty }
+    end
+  end
+end	
