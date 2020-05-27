e6e93a5a2a144e4374edbce1b53e00caae1d2017
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-05-27 00:10:16 -0400

    unfinished

---
+++ linux/system_metrics_spec.rb
@@ -0,0 +1,11 @@
+# based on https://qna.habr.com/q/780041
+context 'system metrics' do
+
+# who -b - последнее включение
+# last -x reboot - в какое время был включен комп
+# last -x shutdown - соответсвенно выключение
+# uptime -p - аптайм системы
+# 
+# journalctl --list-boots
+
+end
