219b45657a6b11e87984d768e66f084a55e875b8
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-07-07 20:13:20 -0400

    intemediate revision - added block format example

--- linux/lsblk_spec.rb
+++ linux/lsblk_spec.rb
@@ -4,17 +4,26 @@ context 'Disk filesystem spec' do
   # https://fibrevillage.com/storage/53-lsblk-command-examples
   # https://gist.github.com/olih/f7437fb6962fb3ee9fe95bda8d2c8fa4
   # https://thoughtbot.com/blog/jq-is-sed-for-json
-  describe command("lsblk -J -f -e 0,11 | jq --slurp '.[]|.blockdevices[]| .name'"), :if => ['debian', 'ubuntu'].include?(os[:family]) do
-    its(:stdout) { should contain 'sda' }
-    # TODO: better querying
+  context 'JSON output format', :if=> ['debian', 'ubuntu'].include?(os[:family]) do
+    describe command("lsblk -J -f -e 0,11 | jq --slurp '.[]|.blockdevices[]| .name'")
+      its(:stdout) { should contain 'sda' }
+    end
+    # https://gist.github.com/olih/f7437fb6962fb3ee9fe95bda8d2c8fa4
+    describe command(<<-EOF
+      lsblk -J -f -e 0,11 | jq --slurp '.[]|.blockdevices[]|.children[]|select(.fstype == "ext4")'
+    EOF
+    ), :if => ['centos', 'ubuntu'].include?(os[:family]) do
+      its(:stdout) { should contain '"fstype": "ext4",' }
+      its(:stdout) { should contain '"mountpoint": "/"' }
+    end
   end
-  # https://gist.github.com/olih/f7437fb6962fb3ee9fe95bda8d2c8fa4
-  describe command(<<-EOF
-    lsblk -J -f -e 0,11 | jq --slurp '.[]|.blockdevices[]|.children[]|select(.fstype == "ext4")'
-  EOF
-  ), :if => ['centos', 'ubuntu'].include?(os[:family]) do
-    its(:stdout) { should contain '"fstype": "ext4",' }
-    its(:stdout) { should contain '"mountpoint": "/"' }
+  context 'Pair output format', :if => ['centos', 'redhat'].include?(os[:family]) do
+    describe command(<<-EOF
+      lsblk -P -f -e 0,11 | | grep 'MOUNTPOINT="/"'
+    EOF
+    )
+      its(:stdout) { should contain 'FSTYPE="xfs"' }
+    end
   end
 end
 
