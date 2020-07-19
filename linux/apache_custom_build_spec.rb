5a0262855a64ead28a95b07b60a327541bb43097
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-07-18 21:39:30 -0400

    intermediate revision - added custom file need to update after move spec

---
+++ linux/apache_custom_build_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+context 'Apache configuration' do
+  # apache built from source after
+  # ./connfigure --prefix=$BASEDIR
+  context 'basedir settings' do
+    # nout counting binary files
+    basedir = '/opt/apache1'
+    %w|
+       bin/envvars
+       bin/apachectl
+       bin/apxs
+       bin/envvars-std
+       bin/apu-1-config
+       bin/apr-1-config
+       conf/original/httpd.conf
+       conf/original/extra/httpd-ssl.conf
+       conf/original/extra/httpd-dav.conf
+       conf/original/extra/httpd-autoindex.conf
+       conf/original/extra/httpd-ssl.conf
+       conf/original/extra/httpd-vhosts.conf
+      |.each do |relative_path|
+      describe file("#{basedir}/#{relative_path}") do
+        its(:content) { should match /(\"|\'|\/)#{Regexp.escape(basedir)}\/\S+/ }
+      end
+    end
+  end
+end
