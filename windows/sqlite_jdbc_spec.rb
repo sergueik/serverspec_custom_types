65c1cc81ec0d72c15e3de4c915728a9b27d17a0f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-09-16 14:20:44 -0400

    intermediate revision - simplified the code that simply hits unused() method

--- windows/sqlite_jdbc_spec.rb
+++ windows/sqlite_jdbc_spec.rb
@@ -34,13 +34,12 @@ context 'JDBC tests' do
       import java.sql.PreparedStatement;
       import java.sql.ResultSet;
       import java.sql.SQLException;
-      import java.sql.SQLFeatureNotSupportedException;
       import java.sql.Statement;
       import java.sql.Blob;
 
       public class #{class_name} {
 
-        public static void main(String[] args) throws ClassNotFoundException {
+        public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
           final String className = "#{jdbc_driver_class_name}";
 
@@ -53,10 +52,10 @@ context 'JDBC tests' do
             connection = DriverManager.getConnection(url);
             Statement statement = connection.createStatement();
             statement.setQueryTimeout(30);
-            ResultSet resultSet = statement.executeQuery("#{query}");
+            ResultSet resultSet = statement.executeQuery("SELECT action_url, username_value, password_value FROM logins");
             while (resultSet.next()) {
-              System.out.println("action_url = " + resultSet.getString("action_url") + "\\n" +
-                                 "username_value = " + resultSet.getString("username_value") + "\\n" +
+              System.out.println("action_url = " + resultSet.getString("action_url") + "\n" + 
+                                 "username_value = " + resultSet.getString("username_value") + "\n" +
                                   "pasword_hash = " + resultSet.getString(4)
                                  );
               // manages to load TEXT columns but fails with BLOB one
@@ -65,20 +64,8 @@ context 'JDBC tests' do
               System.out.println( "password_value = " + blob.getBytes(0, length));
             }
 
-          } catch (SQLFeatureNotSupportedException e) {
-            System.err.println("Exception: " + e.toString());
-          } catch (SQLException e) {
-            System.err.println("Exception: " + e.toString());
-          } catch (Exception e) {
-            System.err.println("Exception: " + e.getMessage());
-          } finally {
-            try {
               if (connection != null)
                 connection.close();
-            } catch (SQLException e) {
-              System.err.println(e);
-            }
-          }
         }
       }
     EOF
@@ -100,6 +87,15 @@ context 'JDBC tests' do
         its(:stdout) { should match Regexp.new(line) }
       end
       its(:stderr) { should_not contain 'java.sql.SQLFeatureNotSupportedException' }
+        # https://github.com/xerial/sqlite-jdbc/blob/master/src/main/java/org/sqlite/jdbc4/JDBC4ResultSet.java#L387
+        # public Blob getBlob(int col) throws SQLException { throw unused(); }
+        # protected SQLException unused() {
+        #   return new SQLFeatureNotSupportedException();
+        # }
+        # Exception in thread "main" java.sql.SQLFeatureNotSupportedException
+        # at org.sqlite.jdbc4.JDBC4ResultSet.unused(JDBC4ResultSet.java:347)
+        # at org.sqlite.jdbc4.JDBC4ResultSet.getBlob(JDBC4ResultSet.java:390)
+        # at Test.main(Test.java:32)
     end
   end
 end
