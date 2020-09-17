4ef1a812cbcaa7c30b252082a147e63c271ec019
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-09-17 01:18:34 -0400

    intermediate revision - added public ports of analytic agent machineagent AppD health check

--- linux/appd_spec.rb
+++ linux/appd_spec.rb
@@ -2,10 +2,23 @@ require 'spec_helper'
 
 # https://docs.appdynamics.com/display/PRO42/Install+the+Java+Agent
 context 'AppDynamics signature spec' do
+  context 'Java agent' do
   service = 'wso2-gateway'
   appdymanics_jar = '/opt/appdynamics/appagent/javaagent.jar'
   describe command("systemctl --no-page -o cat show '#{service}' | grep 'Environment'| sed 's|\\\\x20|\\n|g'") do
     its(:stdout) { should contain "-javaagent:#{appdymanics_jar}" }
   end
   # alternatively scan the jps -v output
+  end
+  # https://community.appdynamics.com/t5/Knowledge-Base/How-do-I-use-the-Analytics-Agent-health-check-URL/ta-p/25783
+  # TODO: examine health check response JSON
+  # curl http://localhost:9091/healthcheck?pretty=true
+  # https://github.com/andrewwardrobe/serverspec-extra-types/blob/master/lib/serverspec_extra_types/types/curl.rb
+  context 'Machine agent' do
+    %w|9090 9091|.each do |p|
+      describe port(p) do
+        it { should be_listening.with('tcp')
+      end
+    end	
+  end
 end
