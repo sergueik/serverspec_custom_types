60bf2ad7e31c81db8eaf10e21769f8de5eb55948
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-09-25 01:34:55 +0200

    intermediate revision - addedseparate test for jar and jar + option checks

--- linux/define_command_wrapper_spec.rb
+++ linux/define_command_wrapper_spec.rb
@@ -1,22 +1,52 @@
 require 'spec_helper'
 
-$DEBUG = true
+$DEBUG = false
 
-context 'Whapping Command in Method test' do
+context 'Wrapping Command in Method test' do
 
-  context 'broken example' do
-      
+  context 'Jar' do
+    def jar_is_running(jar_name )
+      commandline = "test $(ps ax | grep -v grep | grep -c '#{jar_name}') -gt 0"
+      if $DEBUG
+        $stderr.puts "probing #{jar_name}"
+        $stderr.puts "Running command: #{commandline}"
+      end
+      exit_status = command(commandline).exit_status
+      if $DEBUG
+        $stderr.puts "Exit status:#{exit_status}"
+      end
+      status =
+        if exit_status == 0
+          true
+        else
+          false
+        end
+      status
+    end
+
+    jar_name = 'org.apache.catalina.startup.Bootstrap'
+    bogus_jar_name = 'example.Main'
+
+    it "jar #{jar_name} is running" do
+      expect(jar_is_running(jar_name)).to be_truthy
+    end
+    it "jar #{bogus_jar_name} is not running" do
+      expect(jar_is_running(bogus_jar_name)).to be_falsey
+    end
+  end
+  context 'Arguments' do
+
     def jar_is_running_with_argument(jar_name, argument)
       argument.gsub!(/\-/,'\\\\-')
       commandline = "ps ax | grep '#{jar_name}' | grep -v 'grep' | sed 's|  *|\\\\n|g' | grep -q '#{argument}'"
       if $DEBUG
         $stderr.puts "probing #{jar_name} and #{argument}"
         $stderr.puts "Running command: #{commandline}"
-      end  
+      end
       exit_status = command(commandline).exit_status
       if $DEBUG
-      $stderr.puts "Exit status:#{exit_status}"
-      end  
+        $stderr.puts "Exit status:#{exit_status}"
+      end
       status =
         if exit_status == 0
           true
@@ -46,11 +76,12 @@ context 'Whapping Command in Method test' do
     [
       option_missing,
       "-#{option_missing}"
-    ].each do |option|    
+    ].each do |option|
       it "jar #{jar_name} is not running with argument #{option}" do
         expect(jar_is_running_with_argument(jar_name, option)).to be_falsey
       end
     end
   end
 end
+
 
