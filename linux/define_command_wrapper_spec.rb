6ec5de8d05104bcffc256dfcc02c267697a7369f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-09-24 19:08:33 -0400

    interediate revision - added argument processing

--- linux/define_command_wrapper_spec.rb
+++ linux/define_command_wrapper_spec.rb
@@ -1,34 +1,55 @@
 require 'spec_helper'
 
+$DEBUG = true
+
 context 'Whapping Command in Method test' do
+
   context 'broken example' do
-    def jar_is_running(jar_name, argument)
-      $stderr.puts "probing #{jar_name} and #{argument}"
-      $stderr.puts "Running command: ps ax | grep '#{jar_name}' | grep -v 'grep' | sed 's|  *|\\n|g' | grep -q '#{argument}'"
-      exit_status = command("ps ax | grep '#{jar_name}' | grep -v 'grep' | sed 's|  *|\\n|g'| grep -q '#{argument}'").exit_status
+      
+    def jar_is_running_with_argument(jar_name, argument)
+      argument.gsub!(/\-/,'\\\\-')
+      commandline = "ps ax | grep '#{jar_name}' | grep -v 'grep' | sed 's|  *|\\\\n|g' | grep -q '#{argument}'"
+      if $DEBUG
+        $stderr.puts "probing #{jar_name} and #{argument}"
+        $stderr.puts "Running command: #{commandline}"
+      end  
+      exit_status = command(commandline).exit_status
+      if $DEBUG
       $stderr.puts "Exit status:#{exit_status}"
+      end  
       status =
         if exit_status == 0
           true
         else
           false
         end
-
       status
     end
 
     tomcat_version_major = '7'
-    # Centos 7
-    # sudo /usr/sbin/tomcat  start
-    # do not put '-'
-    option_present = 'Dcatalina.base=/usr/share/tomcat'
+
+    # on Centos 7, installs tomcat7 into a non standard directory structure
+    # sudo yum install -q -y tomcat
+    # sudo /usr/sbin/tomcat start
+
     jar_name = 'org.apache.catalina.startup.Bootstrap'
+    option_present = 'Dcatalina.base=/usr/share/tomcat'
     option_missing = 'Dcatalina.base=/opt/tomcat'
-    it "jar #{jar_name} is running with argument #{option_present}" do
-      expect(jar_is_running(jar_name, option_present)).to be_truthy
+    [
+      option_present,
+      "-#{option_present}"
+    ].each do |option|
+      it "jar #{jar_name} is running with argument #{option}" do
+        expect(jar_is_running_with_argument(jar_name, option)).to be_truthy
+      end
     end
-    it "jar #{jar_name} is not running with argument #{option_missing}" do
-      expect(jar_is_running(jar_name, option_missing)).to be_falsey
+    [
+      option_missing,
+      "-#{option_missing}"
+    ].each do |option|    
+      it "jar #{jar_name} is not running with argument #{option}" do
+        expect(jar_is_running_with_argument(jar_name, option)).to be_falsey
+      end
     end
   end
 end
