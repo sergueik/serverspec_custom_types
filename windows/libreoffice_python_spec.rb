7b2c48df51a193c1ef8a1b6caa48bcaedb9b4b34
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-11-25 01:48:44 +0100

    intermediate revision - refactoring and more tricky escape examples for using Libre Office embedded Python on Windows platform

--- windows/libreoffice_python_spec.rb
+++ windows/libreoffice_python_spec.rb
@@ -14,8 +14,9 @@ context 'LibreOffice python' do
       it { should be_file }
     end
   end
+  custom_path = "#{libreoffice_home}\\program;#{libreoffice_home}\\program\\python-core-#{python_version}\\bin"
   describe command 'python -V' do
-    let(:path) { "#{libreoffice_home}\\program;#{libreoffice_home}\\program\\python-core-#{python_version}\\bin" }
+    let(:path) { custom_path }
     its(:exit_status) { should eq 0 }
     its(:stdout) { should contain "Python #{python_version}\\n" }
   end
@@ -23,24 +24,38 @@ context 'LibreOffice python' do
   # NOTE the back slashes
   program = 'print(\\"test\\")'
   describe command "python -c \"#{program}\"" do
-    let(:path) { "#{libreoffice_home}\\program;#{libreoffice_home}\\program\\python-core-#{python_version}\\bin" }
+    let(:path) { custom_path }
     its(:exit_status) { should eq 256 }
     its(:stderr) { should contain 'SyntaxError: unexpected EOF while parsing' }
   end
+  program = "print(\"\"test\"\")"
+  [
+    "python -c \"#{program}\"",
+    "cmd --% /c python -c \"#{program}\""
+  ].each do |command|
+    describe command  do
+      let(:path) { custom_path }
+      its(:exit_status) { should eq 256 }
+      its(:stderr) { should contain "NameError: name 'test' is not defined" }
+    end
+  end
   program = "print('test')"
   describe command "python -c \"#{program}\"" do
-    let(:path) { "#{libreoffice_home}\\program;#{libreoffice_home}\\program\\python-core-#{python_version}\\bin" }
+    let(:path) { custom_path }
     its(:exit_status) { should eq 0 }
     its(:stdout) { should contain 'test' }
   end
-  program = "print(\"\"test\"\")"
-  describe command "python -c \"#{program}\"" do
-    let(:path) { "#{libreoffice_home}\\program;#{libreoffice_home}\\program\\python-core-#{python_version}\\bin" }
-    its(:exit_status) { should eq 256 }
-    its(:stderr) { should contain "NameError: name 'test' is not defined" }
-    
+  program = "print(\\\"\"test\\\"\")"
+  describe command "cmd -- % /c python -c \"#{program}\"" do
+    let(:path) { custom_path }
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should contain 'test' }
   end
   # python -c "help(\"modules\")"
-  # it wasn't my fault
+  # path=%path%;c:\Program Files\LibreOffice\program;c:\Program Files\LibreOffice\program\python-core-3.5.5\bin
+  # python -c "help(""modules"")"
+  # $env:path = "${env:path};c:\Program Files\LibreOffice\program;c:\Program Files\LibreOffice\program\python-core-3.5.5\bin"
+
 end
+
 
