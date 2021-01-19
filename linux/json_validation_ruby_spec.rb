1432a582b087eea8902979c0e91a6b6481df1f98
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-01-19 19:51:01 +0100

    intermediate revision - addd loop over ARGV

--- linux/json_validation_ruby_spec.rb
+++ linux/json_validation_ruby_spec.rb
@@ -1,4 +1,3 @@
-
 require 'spec_helper'
 require 'pp'
 require 'json'
@@ -6,18 +5,18 @@ require 'json'
 
 # basic json syntax validation for 'jq' environment lacking
 context 'json valiation' do
-    datafile = '/tmp/example.json'
-    before(:each) do
-      # NOTE: indent and white space matters
-      Specinfra::Runner::run_command( <<-EOF
-        cat<<END>'#{datafile}'
+  datafile = '/tmp/example.json'
+  before(:each) do
+    # NOTE: indent and white space matters
+    Specinfra::Runner::run_command( <<-EOF
+      cat<<END>'#{datafile}'
 	{"id":"1","name":"test","value":true}
-      EOF
-      )
-    end # NOTE need to escape the # in #{} 
-    # to defer it to be interpereted by the generated Ruby script
-    # NOTE: need to prepend ruby command with uru_rt launcher to i
-    # allow running this snippet in a ruby-sane environment
+    EOF
+    )
+  end # NOTE need to escape the # in #{} 
+  # to defer it to be interpereted by the generated Ruby script
+  # NOTE: need to prepend ruby command with uru_rt launcher to i
+  # allow running this snippet in a ruby-sane environment
   describe command(<<-EOF
     NAME='#{datafile}'
     RUBY='ruby'
@@ -29,6 +28,16 @@ context 'json valiation' do
     its(:exit_status) { should eq 0 }
     its(:stdout) { should contain ('no errors in '+ datafile) }
   end
+  # NOTE: combining postfix notation with for ... end 
+  describe command(<<-EOF
+    NAME='#{datafile}'
+    RUBY='ruby'
+    if [ ! -z  $URU_INVOKER ]; then RUBY='/uru/uru_rt ruby' ; fi
+    $RUBY -rjson -e 'files = [];for arg in ARGV ; filename = arg; files.append filename if JSON.parse(File.read(filename));end; print "no errors in \#{files}"; ' $NAME $NAME
+  EOF
+  ) do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match ('no errors in ["/tmp/example.json", "/tmp/example.json"]' ) }
+    its(:stdout) { should match ("no errors in \[\"#{datafile}\", \"#{datafile}\"\]") }
+  end
 end
-
-
