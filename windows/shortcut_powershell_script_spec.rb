f6f8d1c0e378ffb798fdae4e1e0b36206b934767
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-03-04 00:12:21 -0500

    intermediate revision another possible workaround test

--- windows/shortcut_powershell_script_spec.rb
+++ windows/shortcut_powershell_script_spec.rb
@@ -5,49 +5,98 @@ require 'fileutils'
 $DEBUG = (ENV.fetch('DEBUG', 'false') =~ /^(true|t|yes|y|1)$/i)
 context 'Powerhell Script in Argument Path' do
 
-  cmd_script = 'b.cmd'
-  ps1_script = 'b.ps1'
-  test_dir = 'test dir'
-  message = 'this is a test'
-  script_path = 'c:/users/' + ENV['USER']
-  target_dir =  script_path + '/' + test_dir 
-  dos_path =  target_dir.gsub(/\//,'\\\\')
-  cmd_data = <<-EOF
-    @echo OFF
-    powershell.exe -noprofile -executionpolicy remotesigned "&{ . '#{dos_path}\\#{ps1_script}'}"
-  EOF
-  ps1_data = <<-EOF
-    write-output '#{message}'
-  EOF
-  before(:all) do
-    begin
-      FileUtils.mkdir_p target_dir
-    rescue => e
-      puts e.to_s
-    end
-
-    begin
-      File.open((script_path + '/'+ cmd_script), 'w') do |file|
-        file.write(cmd_data)
+  context 'solution 1' do
+    cmd_script = 'b.cmd'
+    ps1_script = 'b.ps1'
+    test_dir = 'test dir'
+    message = 'this is a test'
+    script_path = 'c:/users/' + ENV['USER']
+    target_dir =  script_path + '/' + test_dir 
+    dos_path =  target_dir.gsub(/\//,'\\\\')
+    cmd_data = <<-EOF
+      @echo OFF
+      powershell.exe -noprofile -executionpolicy remotesigned "&{ . '#{dos_path}\\#{ps1_script}'}"
+    EOF
+    ps1_data = <<-EOF
+      write-output '#{message}'
+    EOF
+    before(:all) do
+      begin
+        FileUtils.mkdir_p target_dir
+      rescue => e
+        puts e.to_s
       end
-    rescue => e
-      puts e.to_s
-    end
 
-    begin
-        File.open((target_dir + '/' + ps1_script), 'w') do |file|
-          file.write(ps1_data)
+      begin
+        File.open((script_path + '/'+ cmd_script), 'w') do |file|
+          file.write(cmd_data)
+        end
+      rescue => e
+        puts e.to_s
       end
-    rescue => e
-      puts e.to_s
+
+      begin
+          File.open((target_dir + '/' + ps1_script), 'w') do |file|
+            file.write(ps1_data)
+        end
+      rescue => e
+        puts e.to_s
+      end
+    end
+    describe command(<<-END_COMMAND
+      pushd '#{script_path}'
+      cmd %%- /c #{cmd_script}
+    END_COMMAND
+    ) do
+      its(:stdout) { should match Regexp.new(message, Regexp::IGNORECASE) }
+      its(:exit_status) {should eq 0}
     end
   end
-  describe command(<<-END_COMMAND
-    pushd '#{script_path}'
-    cmd %%- /c #{cmd_script}
-  END_COMMAND
-  ) do
-    its(:stdout) { should match Regexp.new(message, Regexp::IGNORECASE) }
-    its(:exit_status) {should eq 0}
+  context 'solution 2' do
+    cmd_script = 'c.cmd'
+    ps1_script = 'b.ps1'
+    test_dir = 'test dir'
+    message = 'this is a test'
+    script_path = 'c:/users/' + ENV['USER']
+    target_dir =  script_path + '/' + test_dir 
+    dos_path =  target_dir.gsub(/\//,'\\\\')
+    cmd_data = <<-EOF
+      @echo OFF
+      powershell.exe -noprofile -executionpolicy remotesigned -file "#{dos_path}\\#{ps1_script}"
+    EOF
+    ps1_data = <<-EOF
+      write-output '#{message}'
+    EOF
+    before(:all) do
+      begin
+        FileUtils.mkdir_p target_dir
+      rescue => e
+        puts e.to_s
+      end
+
+      begin
+        File.open((script_path + '/'+ cmd_script), 'w') do |file|
+          file.write(cmd_data)
+        end
+      rescue => e
+        puts e.to_s
+      end
+
+      begin
+          File.open((target_dir + '/' + ps1_script), 'w') do |file|
+            file.write(ps1_data)
+        end
+      rescue => e
+        puts e.to_s
+      end
+    end
+    describe command(<<-END_COMMAND
+      pushd '#{script_path}'
+      cmd %%- /c #{cmd_script}
+    END_COMMAND
+    ) do
+      its(:stdout) { should match Regexp.new(message, Regexp::IGNORECASE) }
+      its(:exit_status) {should eq 0}
+    end
   end
 end
