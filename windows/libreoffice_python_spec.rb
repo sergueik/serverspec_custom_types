8fb329f0e31e8c0d840eaf13376879b724df0d23
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-03-13 01:02:34 +0100

    intermediate revision - bumped the version and fixed few typos

--- windows/libreoffice_python_spec.rb
+++ windows/libreoffice_python_spec.rb
@@ -4,11 +4,11 @@ require_relative '../windows_spec_helper'
 context 'LibreOffice python' do
   # see also: https://github.com/unoconv/unoconv
   #
-  office_verison = '7'
+  office_version = '7'
   embedded_python_versions =
   {
-    '6' => '3.5.5'
-    '7' => '3.7.7'
+    '6' => '3.5.5',
+    '7' => '3.8.4'
   }
   python_version = embedded_python_versions[office_version]
   libreoffice_home = 'c:\Program Files\LibreOffice'
@@ -21,7 +21,7 @@ context 'LibreOffice python' do
       it { should be_file }
     end
   end
-  custom_path = "#{libreoffice_home}\\programi";"#{libreoffice_home}\\program\\python-core-#{python_version}\\bin"
+  custom_path = "#{libreoffice_home}\\program";"#{libreoffice_home}\\program\\python-core-#{python_version}\\bin"
   describe command 'python -V' do
     let(:path) { custom_path }
     its(:exit_status) { should eq 0 }
@@ -38,9 +38,9 @@ context 'LibreOffice python' do
   program = "print(\"\"test\"\")"
   [
     "python -c \"#{program}\"",
-    "cmd --% /c python -c \"#{program}\""
-  ].each do |command|
-    describe command  do
+    "cmd %%- /c python -c \"#{program}\""
+  ].each do |command_line|
+    describe command( command_line ) do
       let(:path) { custom_path }
       its(:exit_status) { should eq 256 }
       its(:stderr) { should contain "NameError: name 'test' is not defined" }
@@ -53,16 +53,31 @@ context 'LibreOffice python' do
     its(:stdout) { should contain 'test' }
   end
   program = "print(\\\"\"test\\\"\")"
-  describe command "cmd -- % /c python -c \"#{program}\"" do
+  describe command "cmd %%-  /c python -c \"#{program}\"" do
     let(:path) { custom_path }
     its(:exit_status) { should eq 0 }
     its(:stdout) { should contain 'test' }
   end
-  # python -c "help(\"modules\")"
-  # path=%path%;c:\Program Files\LibreOffice\program;c:\Program Files\LibreOffice\program\python-core-3.5.5\bin
-  # python -c "help(""modules"")"
-  # $env:path = "${env:path};c:\Program Files\LibreOffice\program;c:\Program Files\LibreOffice\program\python-core-3.5.5\bin"
-
+  context 'wrapper', :if => false do
+    python_launhcer_script = <<-EOF
+      path=%path%;c:\\Program Files\\LibreOffice\\program;c:\\Program Files\\LibreOffice\\program\\python-core-#{python_version}\\bin
+      python -c "help(""modules"")"
+    EOF
+    describe command(<<-EOF
+      write-output '#{python_launhcer_script}'| out-file -filepath "a.cmd" -encoding ASCII
+      start-sleep -millisecond 100
+      cmd %%- /c a.cmd
+    EOF
+    ) do
+      %w|
+      __future__
+      |.each do |line|
+        its(:stdout) { should contain line }
+      end
+      # add a guaranteed to fail to actually see the output
+      its(:stderr) { should be_empty }
+    end  
+    # python -c "help(""modules"")"
+    # $env:path = "${env:path};c:\Program Files\LibreOffice\program;c:\Program Files\LibreOffice\program\python-core-3.5.5\bin"
+  end
 end
-
-
