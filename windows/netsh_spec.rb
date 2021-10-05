a4e458558b878fe7c765cb1447175309a5bdc458
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-10-05 03:26:00 +0200

    intermediate revision - added netst.exe spec with illustrating powershell idiosyncrasies

--- windows/netsh_spec.rb
+++ windows/netsh_spec.rb
@@ -1,15 +1,27 @@
-# origin:https://blogs.msdn.microsoft.com/oldnewthing/20180703-00/?p=99145
+require_relative '../windows_spec_helper'
+context 'Inspecting netsh command' do
+  describe command (<<-EOF
+    $o = netsh.exe interface show interface | select-string -pattern '(enabled|disabled)'|convertfrom-string
+    write-output ([String]::Join(' ', @($o.P4,$o.P5,$o.P6,$o.P7)))
+  EOF
+  ) do
+    its(:stdout) { should contain 'Local Area Connection' }
+  end
+  acl = 'http://+:5985/wsman/'
+  describe command (<<-EOF
+    $acl = '#{acl}'
+    $o = netsh.exe http show urlacl $acl | select-string -pattern 'SDDL'|convertfrom-string
+    start-sleep -millisecond 100
+    $o = netsh.exe http show urlacl $acl | select-string -pattern 'SDDL'|convertfrom-string
+    $result = $o.P3
+    write-output $result
+  EOF
+  ) do
+    its(:stdout) { should match Regexp.new(Regexp.escape('D:(A;;GX;;;S-1-5-80-569256582-2953403351-2909559716-1301513147-412116970)(A;;GX;;;S-1-5-80-4059739203-877974739-1245631912-527174227-2996563517)')) }
+  end
+  # TODO: demonstrate User expectations
+end
+
+# origin: https://blogs.msdn.microsoft.com/oldnewthing/20180703-00/?p=99145
 # NOTE: this test is currenlty a mock up
 
-https://blogs.msdn.microsoft.com/oldnewthing/20180703-00/?p=99145
-
-netsh http
-
-    Reserved URL            : http://+:5985/wsman/
-        User: NT SERVICE\WinRM
-            Listen: Yes
-            Delegate: No
-        User: NT SERVICE\Wecsvc
-            Listen: Yes
-            Delegate: No
-            SDDL: D:(A;;GX;;;S-1-5-80-569256582-2953403351-2909559716-1301513147-412116970)(A;;GX;;;S-1-5-80-4059739203-877974739-1245631912-527174227-2996563517)
