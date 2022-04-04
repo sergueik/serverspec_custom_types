8f82cd203552073d7370bd76dd840b0811a7acf5
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-04-04 02:06:49 +0200

    intermediate revision - added PostgreSQL own SQL through JDBC querty example

--- linux/postgresql_jdbc_spec.rb
+++ linux/postgresql_jdbc_spec.rb
@@ -89,7 +89,7 @@ context 'JDBC tests' do
       its(:stdout) { should contain 'driverObject=class org.postgresql.Driver'}
       its(:stdout) { should contain 'Connected to product: PostgreSQL'}
       its(:stdout) { should match /Connected to catalog: #{database_name}/}
-      its(:stderr) { should_not contain 'Exception: Communications link failure' } # mysql server is not running
+      its(:stderr) { should_not contain 'Exception: Communications link failure' } 
       its(:stderr) { should_not contain 'Exception: Access denied for user' } # configuration mismatch
       its(:stderr) { should_not contain 'Not supported' }
     end
@@ -152,7 +152,94 @@ context 'JDBC tests' do
       its(:stdout) { should contain 'driverObject=class org.postgresql.Driver'}
       its(:stdout) { should contain 'Connected to product: PostgreSQL'}
       its(:stdout) { should match /Connected to catalog: #{database_name}/}
-      its(:stderr) { should_not contain 'Exception: Communications link failure' } # mysql server is not running
+      its(:stderr) { should_not contain 'Exception: Communications link failure' } 
+      its(:stderr) { should_not contain 'Exception: Access denied for user' } # configuration mismatch
+      its(:stderr) { should_not contain 'Not supported' }
+      its(:stderr) { should_not contain 'The server does not support SSL' }
+    end
+  end
+# TODO: before(:all)
+# CREATE TABLE accounts ( id serial PRIMARY KEY, username VARCHAR ( 50 ) UNIQUE NOT NULL, password VARCHAR ( 50 ) NOT NULL, email VARCHAR ( 255 ) UNIQUE NOT NULL, created_on TIMESTAMP NOT NULL, last_login TIMESTAMP  );
+# NOTE: CURRENT_TIMESTAMP() is a function but shoulw be user without parenthesis
+# INSERT INTO accounts (username, password, email, created_on) VALUES ('bob','secret','bob@mail.com',CURRENT_TIMESTAMP);
+#
+# SELECT count(*) <> 0 FROM accounts WHERE username = 'bob';
+#
+  context 'PostgreSQL specific SQL' do
+    class_name = 'PostgreSQLJDBCSQLTest'
+    source_file = "#{class_name}.java"
+    database_name2 = 'template1'
+    # NOTE when a SQL is embedded in the Java code of the test snippet need to write the source file via File class
+
+    source_data = <<-EOF
+      import java.sql.Connection;
+      import java.sql.DriverManager;
+      import java.util.Properties;
+
+      public class #{class_name} {
+        public static void main(String[] argv) throws Exception {
+         String className = "#{jdbc_driver_class_name}";
+         try {
+            Class driverObject = Class.forName(className);
+            System.out.println("driverObject=" + driverObject);
+
+            final String serverName = "#{database_host}";
+            final String databaseName = "#{database_name2}";
+            final String options = "#{options}";
+            // Exception: Communications link failure
+            final String url = "jdbc:#{jdbc_prefix}://" + serverName + "/" + databaseName;
+            final String username = "#{username}";
+            final String password = "#{password}";
+            Properties properties = new Properties();
+            properties.setProperty("user", username);
+            properties.setProperty("password", password);
+            Connection connection = DriverManager.getConnection(url, properties);
+            if (connection != null) {
+              System.out.println("Connected to product: " + connection.getMetaData().getDatabaseProductName());
+              System.out.println("Connected to catalog: " + connection.getCatalog());
+
+              String query = "SELECT count(*) <> 0 FROM accounts WHERE username = 'bob';";
+              System.out.println("Executing count query: " + query);
+              ResultSet resultSet = connection.createStatement().executeQuery(query);
+              resultSet.next();
+              // NOTE: failing with Postgres: resultSet().first()
+              // org.postgresql.util.PSQLException: Operation requires a scrollable ResultSet, but this ResultSet is FORWARD_ONLY
+              final boolean cnt = resultSet.getBoolean(1);
+              System.out.println("count: " + cnt);
+              resultSet.close();
+              connection.close();
+            } else {
+              System.out.println("Failed to connect");
+            }
+          } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+            e.printStackTrace();
+          }
+        }
+      }
+
+    EOF
+    before(:each) do
+      $stderr.puts "Writing #{source_file}"
+      Dir.chdir '/tmp'
+      file = File.open(source_file, 'w')
+      file.puts source_data.strip
+      file.close
+    end
+    describe command(<<-EOF
+      1>/dev/null 2>/dev/null pushd /tmp
+      javac '#{source_file}'
+      java -cp #{jars_cp}#{path_separator}. '#{class_name}'
+      1>/dev/null 2>/dev/null popd
+    EOF
+    ) do
+      its(:exit_status) { should eq 0 }
+
+      its(:stdout) { should contain 'driverObject=class org.postgresql.Driver'}
+      its(:stdout) { should contain 'Connected to product: PostgreSQL'}
+      its(:stdout) { should match /Connected to catalog: #{database_name2}/}
+      its(:stdout) { should match /count: true/}
+      its(:stderr) { should_not contain 'Exception: Communications link failure' } 
       its(:stderr) { should_not contain 'Exception: Access denied for user' } # configuration mismatch
       its(:stderr) { should_not contain 'Not supported' }
       its(:stderr) { should_not contain 'The server does not support SSL' }
