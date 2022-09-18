99cb00c988fb2f56ee4bee434f757dfbaee1ddb0
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-09-19 00:07:11 +0200

    intermediate revision - added homebrewed "file" test

---
+++ linux/filetype_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+# based on https://www.cyberforum.ru/shell/thread3021773.html#post16457085
+# NOTE: not matching the semantics of https://serverspec.org/resource_types.html#file
+context 'File Type' do
+  file = '/opt/vivaldi/vivaldi-bin'
+  describe command("export F='#{file}'; find $F -user root -exec sh -c \"file '{}' | grep -q 'ELF'\" \\; -print") do
+    its(:stdout) { should match Regexp.new(file)}
+  end
+end
+
